d3cc778c692323214059b003bcd8e55558ce6ce8
SumanK307 <sumankumarcse2026@gmail.com>
2024-09-10 18:01:47 +0530

    Add new

--- _01_BASIC/First.java
+++ _01_BASIC/First.java
@@ -1,1 +1,10 @@
+package _01_BASIC;
 
+class First{
+    public static void main(String args[]) {
+        System.out.print("HEllo suman!\n");
+        System.out.println("HEllo suman!");
+        System.out.print("HEllo suman!");
+       
+    }
+}
