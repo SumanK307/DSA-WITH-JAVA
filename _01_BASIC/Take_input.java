82ff0157b7b381dfff843945f0f35473a7828741
SumanK307 <sumankumarcse2026@gmail.com>
2024-09-11 15:57:12 +0530

    Add new

--- _01_BASIC/Take_input.java
+++ _01_BASIC/Take_input.java
@@ -2,7 +2,7 @@ package _01_BASIC;
 import java.util.*;
 public class Take_input {
     public static void main(String args[]) {
-    Scanner sc = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);  // take input from user
     String name1 = sc.next();
     String name2 = sc.nextLine();
     System.out.println(name2);
@@ -12,6 +12,7 @@ public class Take_input {
     float b = sc.nextFloat();
     float sum = a+b;
     System.out.println(sum); 
+    System.out.println(name1+name2); 
    }
 }
 
