712268d1c9515db4a1956c572982144a8447cf86
SumanK307 <sumankumarcse2026@gmail.com>
2024-09-12 22:49:46 +0530

    Add new

---
+++ _01_BASIC/D03_Conditional.java
@@ -0,0 +1,19 @@
+package _01_BASIC;
+
+public class D03_Conditional {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of a:");
+        int a=sc.nextInt();
+        System.out.print("Enter the value of b:");
+        int b=sc.nextInt();
+            if(a>b)
+            {
+                System.out.println(a+" is greater as compare to "+b);
+            }
+            else
+            {
+                System.out.println(b+" is greater as compare to "+a);
+            }
+        }
+}
