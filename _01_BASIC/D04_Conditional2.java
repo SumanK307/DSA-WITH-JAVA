f172471d6247e9f9e24b6e2fd8f5d5d1bc1a421a
SumanK307 <sumankumarcse2026@gmail.com>
2024-09-13 15:53:05 +0530

    Add new

---
+++ _01_BASIC/D04_Conditional2.java
@@ -0,0 +1,14 @@
+package _01_BASIC;
+
+public class D04_Conditional2 {
+    public static void main(String args[]) {
+        int time = 22;
+        if (time < 10) {
+            System.out.println("Good morning.");
+        } else if (time < 18) {
+            System.out.println("Good day.");
+        } else {
+            System.out.println("Good evening.");
+        }
+    }
+}
