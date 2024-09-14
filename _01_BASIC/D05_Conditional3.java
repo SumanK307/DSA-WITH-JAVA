2b68e9b2a1d2ffd8bef51e1291f4170bb121cdd3
SumanK307 <sumankumarcse2026@gmail.com>
2024-09-14 21:26:37 +0530

    Add new

---
+++ _01_BASIC/D05_Conditional3.java
@@ -0,0 +1,11 @@
+package _01_BASIC;
+public class D05_Conditional3 {
+    public static void main(String[] args){
+    int i = 0;
+    while (i < 5) {
+        System.out.println(i);
+        i++;
+    }
+
+    }
+}
