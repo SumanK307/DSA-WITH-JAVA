85dbf265eee8ebb9df6af65452843b6ee4a34dc2
SumanK307 <sumankumarcse2026@gmail.com>
2024-09-15 14:13:52 +0530

    Add new

--- _01_BASIC/D05_While.java
+++ _01_BASIC/D05_While.java
@@ -1,8 +1,7 @@
 package _01_BASIC;
-
 import java.util.*;
 
-public class D05_Conditional3 {
+public class D05_While {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
--- _01_BASIC/D06_DoWhile.java
+++ _01_BASIC/D06_DoWhile.java
@@ -1,15 +1,15 @@
 package _01_BASIC;
+
 import java.util.Scanner;
 
 public class D06_DoWhile {
     public static void main(String[] args) {
-         Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-    int i = 0;
-do {
-  System.out.println(i);
-  i++;
-}
-while (i < 5);
-}
+        int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i < 5);
+    }
 }
