158cbd2bda422153645d2b2b352871947dc4e411
SumanK307 <sumankumarcse2026@gmail.com>
2024-09-15 14:12:32 +0530

    Add new

--- _01_BASIC/D06_DoWhile.java
+++ _01_BASIC/D06_DoWhile.java
@@ -1,7 +1,10 @@
 package _01_BASIC;
+import java.util.Scanner;
 
 public class D06_DoWhile {
     public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
     int i = 0;
 do {
   System.out.println(i);
