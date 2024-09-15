e23f96a20d20e9582ffbca1233eee030d052931a
SumanK307 <sumankumarcse2026@gmail.com>
2024-09-15 14:08:00 +0530

    Add new

--- _01_BASIC/D05_Conditional3.java
+++ _01_BASIC/D05_Conditional3.java
@@ -1,14 +1,16 @@
 package _01_BASIC;
+
 import java.util.*;
+
 public class D05_Conditional3 {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in); 
-        int  n = sc.nextInt();
-    int i=0;
-    while (i < n) {
-        System.out.println(i+" ");
-        i++;
-    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = 0;
+        while (i < n) {
+            System.out.println(i + " ");
+            i++;
+        }
 
     }
 }
