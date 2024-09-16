442c1facb37dd6d4125e40c27189b3eb217094d0
SumanK307 <sumankumarcse2026@gmail.com>
2024-09-16 14:56:46 +0530

    Add new

---
+++ _01_BASIC/D07_ForLoop.java
@@ -0,0 +1,12 @@
+package _01_BASIC;
+import java.util.*;
+public class D07_ForLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = 0;
+        for ( i = 0; i < 5; i++) {
+            System.out.println(i);
+          }
+    }
+}
