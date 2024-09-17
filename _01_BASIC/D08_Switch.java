b05a1795624a72d2c51730c56f8e0d28415100b6
SumanK307 <sumankumarcse2026@gmail.com>
2024-09-17 18:48:28 +0530

    Add new

---
+++ _01_BASIC/D08_Switch.java
@@ -0,0 +1,13 @@
+package _01_BASIC;
+
+import java.util.Scanner;
+
+public class D08_Switch {
+
+public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+}
+
+}
