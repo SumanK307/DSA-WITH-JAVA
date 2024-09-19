dffb8b04b44584be7ee0e433b3f7b59cde70182f
SumanK307 <sumankumarcse2026@gmail.com>
2024-09-19 16:28:31 +0530

    Add new

--- _01_BASIC/D10_ForEach.java
+++ _01_BASIC/D10_ForEach.java
@@ -1,9 +1,10 @@
 package _01_BASIC;
-
+import java.util.*;
 public class D10_ForEach {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
         String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
-        for (String i : cars) {
+        for (String i : cars) { 
             System.out.println(i);
         }
     }
