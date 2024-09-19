0b63c7519fca16a6ad669b25865efa409563584f
SumanK307 <sumankumarcse2026@gmail.com>
2024-09-19 16:26:59 +0530

    Add new

---
+++ _01_BASIC/D10_ForEach.java
@@ -0,0 +1,10 @@
+package _01_BASIC;
+
+public class D10_ForEach {
+    public static void main(String[] args){
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        for (String i : cars) {
+            System.out.println(i);
+        }
+    }
+}
