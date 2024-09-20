c88efd16130daa3f074deb2da08ad530b76ffc0b
SumanK307 <sumankumarcse2026@gmail.com>
2024-09-20 16:24:37 +0530

    Add new

--- _01_BASIC/D11_Continue.java
+++ _01_BASIC/D11_Continue.java
@@ -2,6 +2,11 @@ package _01_BASIC;
 
 public class D11_Continue {
     public static void main(String[] args){
-        
+        for (int i = 0; i < 10; i++) {
+            if (i == 4) {
+              continue;
+            }
+            System.out.println(i);
+          }
     }
 }
