b20e91078256307d90194e3bb647d36a41e007df
SumanK307 <sumankumarcse2026@gmail.com>
2024-09-21 20:51:33 +0530

    Add new

--- _01_BASIC/D12_Break.java
+++ _01_BASIC/D12_Break.java
@@ -1,7 +1,14 @@
 package _01_BASIC;
 
 public class D12_Break {
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+            if (i == 4) {
+                break;
+            }
+        }
     }
 }
