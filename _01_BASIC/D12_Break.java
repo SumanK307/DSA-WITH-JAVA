bc7ab87b7ace2b0bdb3696ab6934b523debf2de8
SumanK307 <sumankumarcse2026@gmail.com>
2024-09-22 21:26:30 +0530

    Add new

--- _01_BASIC/D12_Break.java
+++ _01_BASIC/D12_Break.java
@@ -1,7 +1,8 @@
 package _01_BASIC;
-
+import java.util.*;
 public class D12_Break{
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         int i = 0;
         while (i < 10) {
             System.out.println(i);
