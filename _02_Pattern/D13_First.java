9b528351bd6549a1a27a9cfb5f83575f37e185b1
SumanK307 <sumankumarcse2026@gmail.com>
2024-09-23 20:48:49 +0530

    Add new

---
+++ _02_Pattern/D13_First.java
@@ -0,0 +1,21 @@
+package _02_Pattern;
+
+import java.util.Scanner;
+
+public class D13_First {
+    public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the value of n:");
+    int n = sc.nextInt();
+    int i,j,k;
+    //Q.1 squre
+    //  for(i=0;i<n;i++)
+    // {
+    //     for(j=0;j<n;j++)
+    //     {
+    //         System.out.print(" *");
+    //     }
+    //     System.out.print("\n");
+    // }
+    }
+}
