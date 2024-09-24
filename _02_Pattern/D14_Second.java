60cdaf82f9315c57acfe876b13587ac35f550672
SumanK307 <sumankumarcse2026@gmail.com>
2024-09-24 16:36:31 +0530

    Add new

--- _02_Pattern/D14_Second.java
+++ _02_Pattern/D14_Second.java
@@ -1,7 +1,17 @@
 package _02_Pattern;
+import java.util.Scanner;
 
- public class D14_Second {
+public class D14_Second {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n:");
+        int n = sc.nextInt();
+        int i, j;
+        for (i = 0; i < n; i++) {
+            for (j = i; j < n; j++) {
+                System.out.print(" *");
+            }
+            System.out.print("\n");
+        }
     }
 }
