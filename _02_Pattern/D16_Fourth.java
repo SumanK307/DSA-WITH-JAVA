71715738bb4f77f6c3bfd96cc97590b053cae00c
SumanK307 <sumankumarcse2026@gmail.com>
2024-09-26 15:59:58 +0530

    Add new

---
+++ _02_Pattern/D16_Fourth.java
@@ -0,0 +1,12 @@
+package _02_Pattern;
+import java.util.Scanner;
+
+public class D16_Fourth {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n:");
+        int n = sc.nextInt();
+        int i, j;
+
+    }
+}
