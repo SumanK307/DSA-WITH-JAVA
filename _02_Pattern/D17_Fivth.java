00964fe030c158097aafe591d1e2cba15f33e39c
SumanK307 <sumankumarcse2026@gmail.com>
2024-09-27 12:15:02 +0530

    Add new

--- _02_Pattern/D17_Fivth.java
+++ _02_Pattern/D17_Fivth.java
@@ -1,4 +1,5 @@
-package  _02_Pattern;
+package _02_Pattern;
+
 import java.util.Scanner;
 
 public class D17_Fivth {
@@ -6,7 +7,15 @@ public class D17_Fivth {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the value of n:");
         int n = sc.nextInt();
-        int i, j,k;
-
+        int i, j, k;
+        for (i = 0; i < n; i++) {
+            for (j = n - 1; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (k = i; k > -1; k--) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
     }
 }
