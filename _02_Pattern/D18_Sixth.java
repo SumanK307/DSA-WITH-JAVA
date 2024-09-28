6448497befabf4d9be2f5a4291772f8624d31845
SumanK307 <sumankumarcse2026@gmail.com>
2024-09-28 10:37:17 +0530

    Add new

---
+++ _02_Pattern/D18_Sixth.java
@@ -0,0 +1,11 @@
+package _02_Pattern; 
+import java.util.*;
+
+public class D18_Sixth {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n:");
+        int n = sc.nextInt();
+        int i, j, k;
+    }
+}
