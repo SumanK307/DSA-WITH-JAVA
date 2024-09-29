bb0ae2e6b68ae2c478bcc9a7a6694c5961f165c6
SumanK307 <sumankumarcse2026@gmail.com>
2024-09-29 14:01:35 +0530

    Add new

--- _02_Pattern/D19_seventh.java
+++ _02_Pattern/D19_seventh.java
@@ -1,4 +1,11 @@
 package _02_Pattern;
+import java.util.Scanner;
+
 public class D19_seventh {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n:");
+        int n = sc.nextInt();
+        int i, j, k;
+    }
 }
