bc146f23bfc17b3607040123061630cd7b64d0d9
SumanK307 <sumankumarcse2026@gmail.com>
2024-10-02 09:01:40 +0530

    Add new

--- _02_Pattern/D21_Nine.java
+++ _02_Pattern/D21_Nine.java
@@ -1,5 +1,13 @@
 package _02_Pattern;
 
+import java.util.*;
+
 public class D21_Nine {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n:");
+        int n = sc.nextInt();
+        int i, j, k;
+        k = 0;
+    }
 }
