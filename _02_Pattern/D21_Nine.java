1820f0c484bc88d4589d7ae6c7b259825449fec9
SumanK307 <sumankumarcse2026@gmail.com>
2024-10-03 15:27:24 +0530

    Add new

--- _02_Pattern/D21_Nine.java
+++ _02_Pattern/D21_Nine.java
@@ -8,5 +8,21 @@ public class D21_Nine {
         int n = sc.nextInt();
         int i, j;
         int k = 0;
+        for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            if(i==1|| i==n || j==1 || j==n)
+           {
+            System.out.print("*");
+           } 
+            else
+            {
+                System.out.print(" ");
+            }
+        }   
+        System.out.print("\n");
+        
+    }
     }
 }
