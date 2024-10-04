6962a444c441128efdd66bb1e94df1bb6ac9f833
SumanK307 <sumankumarcse2026@gmail.com>
2024-10-04 09:12:55 +0530

    Add new

--- _02_Pattern/D22_Ten.java
+++ _02_Pattern/D22_Ten.java
@@ -1,4 +1,10 @@
 package _02_Pattern;
+import java.util.Scanner;
 public class D22_Ten {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n:");
+        int n = sc.nextInt();
+        int i, j;
+}
 }
