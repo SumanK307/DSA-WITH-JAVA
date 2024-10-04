b917efd3fda53eba79320b62da7dbdfbaed11a01
SumanK307 <sumankumarcse2026@gmail.com>
2024-10-04 09:16:28 +0530

    Add new

--- _02_Pattern/D22_Ten.java
+++ _02_Pattern/D22_Ten.java
@@ -4,7 +4,30 @@ public class D22_Ten {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the value of n:");
-        int n = sc.nextInt();
-        int i, j;
+        int N = sc.nextInt();
+        int spaces = 2 * N - 1;
+        int stars = 0;
+        for (int i = 1; i <= 2 * N - 1; i++) {
+            if (i <= N) {
+                spaces = spaces - 2;
+                stars++;
+            }
+            else {
+                spaces = spaces + 2;
+                stars--;
+            }
+            for (int j = 1; j <= stars; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= stars; j++) {
+                if (j != N) {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
 }
 }
