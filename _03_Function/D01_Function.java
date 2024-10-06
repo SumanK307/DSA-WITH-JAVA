9411696259d3be3ae84621aeb8b0cc68cd139734
SumanK307 <sumankumarcse2026@gmail.com>
2024-10-06 10:39:12 +0530

    Add new

--- _03_Function/D01_Function.java
+++ _03_Function/D01_Function.java
@@ -3,19 +3,19 @@ package _03_Function;
 import java.util.Scanner;
 
 public class D01_Function {
-    public static int calculateSum(int a , int b){
-        int sum = a+b;
+    public static int calculateSum(int a, int b) {
+        int sum = a + b;
         return sum;
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number:");
         int n = sc.nextInt();
-        int a=sc.nextInt();
-        int b=sc.nextInt();
-        int sum=calculateSum(a,b);
-        System.out.println("Sum of 2 numbers is:"+ sum);
-        System.out.println("Sum of 2 numbers is:"+ calculateSum(a,b)) ;
-        //printFactorial(n);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = calculateSum(a, b);
+        System.out.println("Sum of 2 numbers is:" + sum);
+        //System.out.println("Sum of 2 numbers is:" + calculateSum(a, b));
     }
 }
