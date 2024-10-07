a25af1abd9a2376ed072f506b74f00c2db66cad8
SumanK307 <sumankumarcse2026@gmail.com>
2024-10-07 14:25:14 +0530

    Add new

--- _03_Function/D02_Second.java
+++ _03_Function/D02_Second.java
@@ -3,11 +3,26 @@ package _03_Function;
 import java.util.Scanner;
 
 public class D02_Second {
+    public static void printFactorial(int n) {
+        if (n < 0) {
+            System.out.print("invalid");
+            return;
+        } else if (n == 0) {
+            System.out.println("factorial=1");
+            return;
+        }
+        int factorial = 1, i;
+        for (i = 1; i <= n; i++) {
+            factorial = factorial * i;
+        }
+        System.out.println(factorial);
+        return;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number:");
         int n = sc.nextInt();
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        printFactorial(n);
     }
 }
