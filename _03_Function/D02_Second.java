7f9e662938337f365419c92104c2c171d0487605
SumanK307 <sumankumarcse2026@gmail.com>
2024-10-07 14:24:01 +0530

    Add new

--- _03_Function/D02_Second.java
+++ _03_Function/D02_Second.java
@@ -1,5 +1,13 @@
 package _03_Function;
 
+import java.util.Scanner;
+
 public class D02_Second {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number:");
+        int n = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+    }
 }
