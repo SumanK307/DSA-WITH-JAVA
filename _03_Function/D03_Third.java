fa8b6e2d2ae48cdf7c41168b34f4fba626e06093
SumanK307 <sumankumarcse2026@gmail.com>
2024-10-08 10:18:13 +0530

    Add new

--- _03_Function/D03_Third.java
+++ _03_Function/D03_Third.java
@@ -1,5 +1,9 @@
 package _03_Function;
-
+import java.util.Scanner;
 public class D03_Third {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number:");
+        int n = sc.nextInt();
+    }
 }
