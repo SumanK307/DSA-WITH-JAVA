f61c61a2d24b8da7a5c113a85b15fe848eac4ce1
SumanK307 <sumankumarcse2026@gmail.com>
2024-10-08 10:20:49 +0530

    Add new

--- _03_Function/D03_Third.java
+++ _03_Function/D03_Third.java
@@ -1,9 +1,22 @@
 package _03_Function;
+
 import java.util.Scanner;
+
 public class D03_Third {
+    public static void greaterNo(int a, int b) {
+        int total = a;
+        for (int i = 2; i <= b; i++) {
+            total = total * a;
+        }
+        System.out.println(total);
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number:");
-        int n = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        greaterNo(a, b);
+
     }
 }
