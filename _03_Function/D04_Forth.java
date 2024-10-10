877e56e76f1856e4e89f8b91ee3f1074a9ad501f
SumanK307 <sumankumarcse2026@gmail.com>
2024-10-10 22:11:19 +0530

    Add new

--- _03_Function/D04_Forth.java
+++ _03_Function/D04_Forth.java
@@ -1,4 +1,9 @@
 package _03_Function;
 public class D04_Forth {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number:");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+    }
 }
