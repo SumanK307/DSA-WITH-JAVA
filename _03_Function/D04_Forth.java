885a056e93aeac86d83af478f886e3b2330b6be1
SumanK307 <sumankumarcse2026@gmail.com>
2024-10-11 21:44:14 +0530

    Add new

--- _03_Function/D04_Forth.java
+++ _03_Function/D04_Forth.java
@@ -1,10 +1,23 @@
 package _03_Function;
 import java.util.Scanner;
 public class D04_Forth {
-    public static void main(String[] args) {
+    static void Details(int marks) {
+        if (marks < 35) {
+            System.out.println("Fail");
+        } else if (marks >= 35 && marks < 65) {
+            System.out.println("B grade");
+        } else if (marks >= 65 && marks < 75) {
+            System.out.println("A grade");
+        } else if (marks >= 75 && marks < 100) {
+            System.out.println("O grade");
+        } else {
+            System.out.println("Invalid marks entered");
+        } 
+    }
+        public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number:");
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        int marks = sc.nextInt();
+        
     }
 }
