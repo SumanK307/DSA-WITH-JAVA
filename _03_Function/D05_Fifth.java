ff4c31b5231d25b54af81b198f3e9ab81ef87374
SumanK307 <sumankumarcse2026@gmail.com>
2024-10-12 16:07:25 +0530

    Add new

--- _03_Function/D05_Fifth.java
+++ _03_Function/D05_Fifth.java
@@ -1,5 +1,11 @@
 package _03_Function;
+
+import java.util.Scanner;
+
 public class D05_Fifth {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number:");
+        int marks = sc.nextInt();
     }
 }
