44d2913f063596b516c9d902653ca500b5376d3b
SumanK307 <sumankumarcse2026@gmail.com>
2024-10-13 10:53:16 +0530

    Add new

--- _03_Function/D05_Fifth.java
+++ _03_Function/D05_Fifth.java
@@ -2,6 +2,22 @@ package _03_Function;
 import java.util.Scanner;
 
 public class D05_Fifth {
+    public static String middle(String str)
+    {
+        int position;
+        int length;
+        if (str.length() % 2 == 0)
+        {
+            position = str.length() / 2 - 1;
+            length = 2;
+        }
+        else
+        {
+            position = str.length() / 2;
+            length = 1;
+        }
+        return str.substring(position, position + length);
+    }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Input a string: ");
