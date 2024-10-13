f3c36ab750ffe536a92845884a73be37a1c7db15
SumanK307 <sumankumarcse2026@gmail.com>
2024-10-13 10:53:26 +0530

    Add new

--- _03_Function/D05_Fifth.java
+++ _03_Function/D05_Fifth.java
@@ -1,28 +1,26 @@
 package _03_Function;
+
 import java.util.Scanner;
 
 public class D05_Fifth {
-    public static String middle(String str)
-    {
+    public static String middle(String str) {
         int position;
         int length;
-        if (str.length() % 2 == 0)
-        {
+        if (str.length() % 2 == 0) {
             position = str.length() / 2 - 1;
             length = 2;
-        }
-        else
-        {
+        } else {
             position = str.length() / 2;
             length = 1;
         }
         return str.substring(position, position + length);
     }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Input a string: ");
         String str = in.nextLine();
-        System.out.print("The middle character in the string: " + middle(str)+"\n");
+        System.out.print("The middle character in the string: " + middle(str) + "\n");
 
     }
 }
