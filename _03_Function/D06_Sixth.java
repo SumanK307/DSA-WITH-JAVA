a950f29a8e6b8219cdf944b7872f7061fd81f5ba
SumanK307 <sumankumarcse2026@gmail.com>
2024-10-14 16:55:55 +0530

    Add new

--- _03_Function/D06_Sixth.java
+++ _03_Function/D06_Sixth.java
@@ -6,6 +6,6 @@ public class D06_Sixth {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Input a string: ");
-    
+        String str = in.nextLine();
     }
 }
