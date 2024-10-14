7958e7cc959ab22c33703caeee473399913de20c
SumanK307 <sumankumarcse2026@gmail.com>
2024-10-14 16:46:20 +0530

    Add new

--- _03_Function/D06_Sixth.java
+++ _03_Function/D06_Sixth.java
@@ -1,5 +1,11 @@
 package _03_Function;
 
+import java.util.Scanner;
+
 public class D06_Sixth {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input a string: ");
     
+    }
 }
