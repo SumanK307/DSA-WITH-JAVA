542f9e07e7c0196bdc755df55b0e83c96a39513f
SumanK307 <sumankumarcse2026@gmail.com>
2024-10-15 22:42:49 +0530

    Add new

--- _03_Function/D06_Sixth.java
+++ _03_Function/D06_Sixth.java
@@ -3,9 +3,21 @@ package _03_Function;
 import java.util.Scanner;
 
 public class D06_Sixth {
+    void test() {
+        int n = 5732, result = 0;
+        do {
+            result *= 10;
+            int digit = n % 10;
+            result += digit;
+            n = n / 10;
+        } while (n > 0);
+        System.out.println("Output=" + result);
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Input a string: ");
         String str = in.nextLine();
+
     }
 }
