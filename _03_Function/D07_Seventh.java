789a5e6dfc75509965ce351e21d2f1cec20b2c0f
SumanK307 <sumankumarcse2026@gmail.com>
2024-10-16 12:05:49 +0530

    Add new

--- _03_Function/D07_Seventh.java
+++ _03_Function/D07_Seventh.java
@@ -1,7 +1,17 @@
 package _03_Function;
 import java.util.Scanner;
 public class D07_Seventh {
+    void compare(char c1, char c2)
+    {
+        if ( (int)c1 > (int)c2 )
+            System.out.println("Largest character from "+c1+ " and "+c2+" is= "+c1);
+        else if((int)c2 > (int)c1)
+            System.out.println("Largest character from "+c1+ " and "+c2+ " is= "+c2);
+        else
+            System.out.println("Both the characters "+c1+ " and "+c2+ " are same and value is= "+c1);
+    }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+
     }
 }
