729dc349fd1c75bca1bc6c6d9d5d875d943b22ca
SumanK307 <sumankumarcse2026@gmail.com>
2024-10-16 12:03:39 +0530

    Add new

--- _03_Function/D07_Seventh.java
+++ _03_Function/D07_Seventh.java
@@ -1,7 +1,7 @@
 package _03_Function;
-
+import java.util.Scanner;
 public class D07_Seventh {
     public static void main(String[] args) {
-    
+        Scanner in = new Scanner(System.in);
     }
 }
