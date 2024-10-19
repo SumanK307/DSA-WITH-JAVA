cb3df9c9bb04660fa865ee3583f772b989943f30
SumanK307 <sumankumarcse2026@gmail.com>
2024-10-19 17:05:11 +0530

    Add new

--- _03_Function/D08_Eight.java
+++ _03_Function/D08_Eight.java
@@ -3,15 +3,11 @@ package _03_Function;
 import java.util.Scanner;
 
 public class D08_Eight {
-    static void Fibonacci(int N)
+    static void Fibonacci(int n)
     {
         int num1 = 0, num2 = 1;
-
-        for (int i = 0; i < N; i++) {
-            // Print the number
+        for (int i = 0; i < n; i++) {
             System.out.print(num1 + " ");
-
-            // Swap
             int num3 = num2 + num1;
             num1 = num2;
             num2 = num3;
