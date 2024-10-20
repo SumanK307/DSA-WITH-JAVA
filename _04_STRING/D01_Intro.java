2deed3b6e45c9ba3b8048d59286651c0d30d6ee0
SumanK307 <sumankumarcse2026@gmail.com>
2024-10-20 13:17:02 +0530

    Add new

--- _04_STRING/D01_Intro.java
+++ _04_STRING/D01_Intro.java
@@ -1,5 +1,10 @@
 package _04_STRING;
 
+import java.util.Scanner;
+
 public class D01_Intro {
-    
+    Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n:");
+        int n = sc.nextInt();
+}
 }
