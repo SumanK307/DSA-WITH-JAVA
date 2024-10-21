a00b0e52ee83ff685c9fa893e8a54d7e84a80295
SumanK307 <sumankumarcse2026@gmail.com>
2024-10-21 17:03:56 +0530

    Add new

--- _04_STRING/D01_Intro.java
+++ _04_STRING/D01_Intro.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class D01_Intro {
     public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
-        System.out.println("Enter any String:");
+        System.out.println("Enter a String:");
         String n = sc.nextLine();
         System.out.println(n);
         System.out.println("Enter any String:");
