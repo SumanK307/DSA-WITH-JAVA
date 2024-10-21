ecd6f67c1582aff51f327e6b061743a47eef4386
SumanK307 <sumankumarcse2026@gmail.com>
2024-10-21 17:04:16 +0530

    Add new

--- _04_STRING/D01_Intro.java
+++ _04_STRING/D01_Intro.java
@@ -3,19 +3,19 @@ package _04_STRING;
 import java.util.Scanner;
 
 public class D01_Intro {
-    public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter a String:");
         String n = sc.nextLine();
         System.out.println(n);
         System.out.println("Enter a new String:");
         String m = sc.next();
         System.out.println(m);
-        String name="suman";
-        String fullName="suman kumar";
-        String sentance="my name is suman kumar:";
+        String name = "suman";
+        String fullName = "suman kumar";
+        String sentance = "my name is suman kumar:";
         System.out.println(name);
         System.out.println(fullName);
         System.out.println(sentance);
-}
+    }
 }
