28d2010bc463621bf8cd8532e90d25d1a178b91a
SumanK307 <sumankumarcse2026@gmail.com>
2024-10-21 17:02:49 +0530

    Add new

--- _04_STRING/D01_Intro.java
+++ _04_STRING/D01_Intro.java
@@ -11,5 +11,11 @@ public class D01_Intro {
         System.out.println("Enter any String:");
         String m = sc.next();
         System.out.println(m);
+        String name="suman";
+        String fullName="suman kumar";
+        String sentance="my name is suman kumar:";
+        System.out.println(name);
+        System.out.println(fullName);
+        System.out.println(sentance);
 }
 }
