658e32e6a5be932810c772b1219d5321bfd6f3b1
SumanK307 <sumankumarcse2026@gmail.com>
2024-10-22 18:26:28 +0530

    Add new

--- _04_STRING/D02_Second.java
+++ _04_STRING/D02_Second.java
@@ -1,4 +1,9 @@
 package _04_STRING;
+
+import java.util.Scanner;
+
 public class D02_Second {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
 }
