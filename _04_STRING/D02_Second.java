e9fd11383db661f9e19e143925778dae915c799d
SumanK307 <sumankumarcse2026@gmail.com>
2024-10-23 02:09:48 +0530

    Add new

--- _04_STRING/D02_Second.java
+++ _04_STRING/D02_Second.java
@@ -1,11 +1,10 @@
 package _04_STRING;
-
 import java.util.Scanner;
 
 public class D02_Second {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a String:");
+        System.out.println("Enter a new String:");
         String n = sc.nextLine();
     }
 }
