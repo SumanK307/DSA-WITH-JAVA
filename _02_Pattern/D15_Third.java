ed17a26dc3690ba94bc3815b3d127ac61b5bbd19
SumanK307 <sumankumarcse2026@gmail.com>
2024-10-24 18:29:21 +0530

    Add new

--- _02_Pattern/D15_Third.java
+++ _02_Pattern/D15_Third.java
@@ -7,7 +7,7 @@ public class D15_Third {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the value of n:");
         int n = sc.nextInt();
-        int i, j;
+        int i,j;
         for (i = 0; i < n; i++) {
             for (j = 0; j < i; j++) {
                 System.out.print(" *");
