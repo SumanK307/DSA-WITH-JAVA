83db0b80d9ca98232d1e4fcb90defdf76fab8afe
SumanK307 <sumankumarcse2026@gmail.com>
2024-10-28 22:06:45 +0530

    Add new

--- _02_Pattern/D20_Eight.java
+++ _02_Pattern/D20_Eight.java
@@ -1,4 +1,5 @@
 package _02_Pattern;
+
 import java.util.Scanner;
 
 public class D20_Eight {
@@ -7,22 +8,17 @@ public class D20_Eight {
         System.out.print("Enter the number value:");
         int n = sc.nextInt();
         int i, j, k;
-    k=0;
-    for(i=1;i<n;i++)
-    {
-        for(j=1;j<i;j++)
-        {
-            k++;
-            if(k%2==0)
-            {
-                System.out.print("0");
+        k = 0;
+        for (i = 1; i < n; i++) {
+            for (j = 1; j < i; j++) {
+                k++;
+                if (k % 2 == 0) {
+                    System.out.print("0");
+                } else {
+                    System.out.print("1");
+                }
             }
-            else
-            {
-                System.out.print("1");
-            }
+            System.out.print("\n");
         }
-        System.out.print("\n");
     }
-}
 }
