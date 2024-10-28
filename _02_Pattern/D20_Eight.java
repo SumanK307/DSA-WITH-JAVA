1e9bb3e23c0b464f572dea37a1948eae7ff46dbb
SumanK307 <sumankumarcse2026@gmail.com>
2024-10-28 22:04:30 +0530

    Add new

--- _02_Pattern/D20_Eight.java
+++ _02_Pattern/D20_Eight.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class D20_Eight {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the value of n:");
+        System.out.print("Enter t:");
         int n = sc.nextInt();
         int i, j, k;
     k=0;
