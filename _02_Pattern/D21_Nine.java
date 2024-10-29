09adc58f7d5037347b8f4efa5242aa0464d89380
SumanK307 <sumankumarcse2026@gmail.com>
2024-10-29 22:46:37 +0530

    Add new

--- _02_Pattern/D21_Nine.java
+++ _02_Pattern/D21_Nine.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class D21_Nine {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the value of n:");
+        System.out.print("Enter the value of :");
         int n = sc.nextInt();
         int i, j;
         for (i = 1; i <= n; i++) {
