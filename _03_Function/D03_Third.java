0b69cdc0b3dd220bb3c996a99cfe36045582ebbb
SumanK307 <sumankumarcse2026@gmail.com>
2024-11-01 22:07:20 +0530

    Add new

--- _03_Function/D03_Third.java
+++ _03_Function/D03_Third.java
@@ -11,7 +11,7 @@ public class D03_Third {
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number:");
+        System.out.print("Enter the :");
         int a = sc.nextInt();
         int b = sc.nextInt();
         greaterNo(a, b);
