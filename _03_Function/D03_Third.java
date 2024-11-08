bcad61222cf92ed46cbd33fd77ef35a60cb640f4
SumanK307 <sumankumarcse2026@gmail.com>
2024-11-08 20:32:25 +0530

    Add new

--- _03_Function/D03_Third.java
+++ _03_Function/D03_Third.java
@@ -2,7 +2,7 @@ package _03_Function;
 import java.util.Scanner;
 
 public class D03_Third {
-    public static void greaterNo(int a, int b) {
+    public static void greaterNo(int a,int b) {
         int total = a;
         for (int i = 2; i <= b; i++) {
             total = total * a;
