a43420721e6514aca5d8329dc7b543cdc332e3bc
SumanK307 <sumankumarcse2026@gmail.com>
2024-11-09 20:42:10 +0530

    Add new

--- _03_Function/D04_Forth.java
+++ _03_Function/D04_Forth.java
@@ -9,7 +9,8 @@ public class D04_Forth {
         } 
         else if (marks >= 35 && marks < 65) {
             System.out.println("B grade");
-        } else if (marks >= 65 && marks < 75) {
+        } 
+        else if (marks >= 65 && marks < 75) {
             System.out.println("A grade");
         } else if (marks >= 75 && marks < 100) {
             System.out.println("O grade!");
