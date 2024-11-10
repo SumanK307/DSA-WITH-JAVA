d1f8dae6fd17887ebb8635e5095157efc7378158
SumanK307 <sumankumarcse2026@gmail.com>
2024-11-10 22:12:51 +0530

    Add new

--- _03_Function/D05_Fifth.java
+++ _03_Function/D05_Fifth.java
@@ -18,7 +18,7 @@ public class D05_Fifth {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Input a string:");
+        System.out.print("Input a string: ");
         String str = in.nextLine();
         System.out.print("The middle character in the string: " + middle(str) + "\n");
 
