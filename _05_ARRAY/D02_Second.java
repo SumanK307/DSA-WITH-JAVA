22a6bee2d769b5055d1a066c30b40555df85569c
SumanK307 <sumankumarcse2026@gmail.com>
2024-11-16 23:35:59 +0530

    Add new

---
+++ _05_ARRAY/D02_Second.java
@@ -0,0 +1,17 @@
+package _05_ARRAY;
+import java.util.*;
+
+public class D02_Second {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter row no:");
+        int rows= sc.nextInt();
+        System.out.print("Enter column no:");
+        int cols= sc.nextInt();
+        int numbers[][]= new int[rows][cols];
+     System.out.print("Enter Element:");
+    
+    }
+}
+
+
