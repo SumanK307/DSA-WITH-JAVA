b5a64c3c588eef8501086ab5c49d89c119b21717
SumanK307 <sumankumarcse2026@gmail.com>
2024-11-16 23:37:25 +0530

    Add new

--- _05_ARRAY/D02_Second.java
+++ _05_ARRAY/D02_Second.java
@@ -1,32 +1,28 @@
 package _05_ARRAY;
+
 import java.util.*;
 
 public class D02_Second {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter row no:");
-        int rows= sc.nextInt();
+        int rows = sc.nextInt();
         System.out.print("Enter column no:");
-        int cols= sc.nextInt();
-        int numbers[][]= new int[rows][cols];
-     System.out.print("Enter Element:");
-     for(int i=0;i<rows;i++)
-     {
-         for(int j=0;j<cols;j++)
-         {
-             numbers[i][j]=sc.nextInt();
-         }
-     }
-      System.out.println("Print Element:");
-      for(int i=0;i<rows;i++)
-      {   System.out.print(" ");
-          for(int j=0;j<cols;j++)
-          {
-              System.out.print(numbers[i][j] + "  ");
-          }
-        System.out.println();
-      }
+        int cols = sc.nextInt();
+        int numbers[][] = new int[rows][cols];
+        System.out.print("Enter Element:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                numbers[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Print Element:");
+        for (int i = 0; i < rows; i++) {
+            System.out.print(" ");
+            for (int j = 0; j < cols; j++) {
+                System.out.print(numbers[i][j] + "  ");
+            }
+            System.out.println();
+        }
     }
 }
-
-
