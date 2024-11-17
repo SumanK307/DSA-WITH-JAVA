1aa16567640ba1f197575b66c438b3dd68a5ad08
SumanK307 <sumankumarcse2026@gmail.com>
2024-11-17 21:57:00 +0530

    Add new

--- _05_ARRAY/D03_Third.java
+++ _05_ARRAY/D03_Third.java
@@ -5,5 +5,6 @@ import java.util.Scanner;
 public class D03_Third {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int[] arr = new int[4];
     }
 }
