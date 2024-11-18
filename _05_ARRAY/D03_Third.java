2c36e86d2b806c4e77bdb11235f326833d8978b2
SumanK307 <sumankumarcse2026@gmail.com>
2024-11-18 19:19:14 +0530

    Add new

--- _05_ARRAY/D03_Third.java
+++ _05_ARRAY/D03_Third.java
@@ -8,5 +8,15 @@ public class D03_Third {
         System.out.print("Enter NO:");
      int n = sc.nextInt();
      int[] arr = new int[5];
+     // int[] arr_2 =arr;  //Copy array and locate same memory of arr
+    // arr_2[1]=23;       // arr_2 and arr both have 23 at index 1
+    // int[] arr_2 =arr.clone();  //Copy array and not locate same memory of arr
+    // int[] arr_2 =Arrays.copyOf(arr,arr.length);     //Copy array and not locate same memory of arr
+    // Arrays.copyOfRange(arr , StartingIndex ,EndingIndex) ;
+    System.out.println("Enter Element:");
+    for(int i=0; i<n;i++)          //n=arr.length;
+    {
+        arr[i] = sc.nextInt();
+    }
     }
 }
