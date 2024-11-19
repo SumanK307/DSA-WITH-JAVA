92b886980c94e3abe86dc1f1611883b3a98c0dfe
SumanK307 <sumankumarcse2026@gmail.com>
2024-11-19 12:40:19 +0530

    Add new

--- _05_ARRAY/D04_Four.java
+++ _05_ARRAY/D04_Four.java
@@ -1,7 +1,12 @@
 package _05_ARRAY;
 
+import java.util.Scanner;
+
 public class D04_Four {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter NO:");
+     int n = sc.nextInt();
+     int[] arr = new int[5];
     }
 }
