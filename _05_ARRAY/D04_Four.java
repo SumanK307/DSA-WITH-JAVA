2b8d7d45bfb73c3f015b9734ff7c61e2a8c75237
SumanK307 <sumankumarcse2026@gmail.com>
2024-11-20 16:13:08 +0530

    Add new

--- _05_ARRAY/D04_Four.java
+++ _05_ARRAY/D04_Four.java
@@ -5,8 +5,14 @@ import java.util.Scanner;
 public class D04_Four {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter NO:");
-     int n = sc.nextInt();
-     int[] arr = new int[5];
+    //     System.out.print("Enter NO:");
+    //  int n = sc.nextInt();
+    //  int[] arr = new int[5];
+    System.out.print("Enter row no:");
+        int rows = sc.nextInt();
+        System.out.print("Enter column no:");
+        int cols = sc.nextInt();
+        int arr1[][] = new int[rows][cols];
+        int arr2[][] = new int[rows][cols];
     }
 }
