928424af915505efabbba161cb5b9af42e15bd0a
SumanK307 <sumankumarcse2026@gmail.com>
2024-11-26 22:15:20 +0530

    Add new

--- _05_ARRAY/D05_Five.java
+++ _05_ARRAY/D05_Five.java
@@ -7,5 +7,8 @@ public class D05_Five {
         System.out.print(" Enter size of the Array: ");
         int n = sc.nextInt();
         int [] arr = new int[n];
+        System.out.print(" Enter " + n + " Elements: "); 
+        for(int i=0; i<n; i++)
+            arr[i] = sc.nextInt();
     }
 }
