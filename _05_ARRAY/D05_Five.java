d3196c7397f4b6e2cf306881b7aba3fcd408f42d
SumanK307 <sumankumarcse2026@gmail.com>
2024-11-26 22:05:39 +0530

    Add new

--- _05_ARRAY/D05_Five.java
+++ _05_ARRAY/D05_Five.java
@@ -3,6 +3,9 @@ import java.util.*;
 
 public class D05_Five {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.print(" Enter size of the Array: ");
+        int n = sc.nextInt();
+        int [] arr = new int[n];
     }
 }
