df6a51de6c15aacb747face8bbe8a88de7605c48
SumanK307 <sumankumarcse2026@gmail.com>
2024-11-27 23:29:07 +0530

    Add new

--- _05_ARRAY/D05_Five.java
+++ _05_ARRAY/D05_Five.java
@@ -7,7 +7,13 @@ public class D05_Five {
     static int search(int[] arr, int target){
         int start = 0;
         int end   = arr.length-1; 
-
+        while(start <= end){
+            // find mid
+            int mid = start + (end - start)/2; 
+            if(arr[mid] == target)
+                return mid;
+            else if(arr[mid]> target)
+                end = mid-1;
 
     }
     public static void main(String[] args) {
