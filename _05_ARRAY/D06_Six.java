cd1cbd47b629518a01569e132f7b952eb8be3b88
SumanK307 <sumankumarcse2026@gmail.com>
2024-12-01 23:29:06 +0530

    Add new

--- _05_ARRAY/D06_Six.java
+++ _05_ARRAY/D06_Six.java
@@ -1,7 +1,12 @@
 package _05_ARRAY;
 
+import java.util.Scanner;
+
 public class D06_Six {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n:");  
+        int n = sc.nextInt();
+        System.out.println("Squre root of "+n+" is :"+SqureRoot(n));
     }
 }
