4734c60a62d46122aa7b94a8d37443e2b755f34f
SumanK307 <sumankumarcse2026@gmail.com>
2024-12-01 23:30:11 +0530

    Add new

--- _05_ARRAY/D06_Six.java
+++ _05_ARRAY/D06_Six.java
@@ -3,6 +3,26 @@ package _05_ARRAY;
 import java.util.Scanner;
 
 public class D06_Six {
+    static int SqureRoot(int n){
+        int s=0;
+        int e=n;
+        int mid;
+        int ans=-1;
+        while(s<=e){
+            mid=s+(e-s)/2;
+            if(n*n==n){
+                return mid;
+            }
+            else if(n*n<n){
+                ans=mid;
+                s=mid+1;
+            }
+            else{
+                e=mid-1;
+            }
+        }
+        return ans;
+        }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the value of n:");  
