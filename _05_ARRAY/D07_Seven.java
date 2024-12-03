dc41ff84c92773a8ae5260d2aa5dbaf12785b0b3
SumanK307 <sumankumarcse2026@gmail.com>
2024-12-03 23:11:12 +0530

    Add new

--- _05_ARRAY/D07_Seven.java
+++ _05_ARRAY/D07_Seven.java
@@ -1,7 +1,12 @@
 package _05_ARRAY;
 
+import java.util.Scanner;
+
 public class D07_Seven {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+        System.out.print("Enter the size of array:");
+        int n=sc.nextInt();
     }
 }
