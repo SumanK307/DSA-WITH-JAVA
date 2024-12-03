3233fd2d502c41b4e07eed5973b8fbc43681ec44
SumanK307 <sumankumarcse2026@gmail.com>
2024-12-03 23:12:47 +0530

    Add new

--- _05_ARRAY/D07_Seven.java
+++ _05_ARRAY/D07_Seven.java
@@ -3,6 +3,24 @@ package _05_ARRAY;
 import java.util.Scanner;
 
 public class D07_Seven {
+    static void printArray(int[] arr ,int n){
+        for (int i = 0; i < n; i++) {
+            System.out.print(" "+ arr[i]);
+        }
+        System.out.println();
+    }
+//**********************Find two element sum equal to target****************************/
+    static int Count(int[] arr,int s){
+        int c=0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if(arr[i]+arr[j]==s){
+                    c++;
+                }
+            }
+        }
+        return c;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] arr = new int[10];
