ca60a4025a2279124765452ef29ee21a7b7c2f10
SumanK307 <sumankumarcse2026@gmail.com>
2024-12-03 23:11:56 +0530

    Add new

--- _05_ARRAY/D07_Seven.java
+++ _05_ARRAY/D07_Seven.java
@@ -8,5 +8,9 @@ public class D07_Seven {
         int[] arr = new int[10];
         System.out.print("Enter the size of array:");
         int n=sc.nextInt();
+        System.out.print("Enter Array element:");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
     }
 }
