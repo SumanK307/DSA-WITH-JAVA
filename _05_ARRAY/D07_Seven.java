afda6b2188dee98ace5776a02919f5a5e0fd2d6a
SumanK307 <sumankumarcse2026@gmail.com>
2024-12-04 22:11:30 +0530

    Add new

--- _05_ARRAY/D07_Seven.java
+++ _05_ARRAY/D07_Seven.java
@@ -25,7 +25,8 @@ public class D07_Seven {
     //**********************Find two element sum equal to target****************************/
     static int Count(int[] arr,int s){
         int c=0;
-
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length; j++) {
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
