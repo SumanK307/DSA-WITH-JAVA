78fb8076e0244c1709511e3536aa5b385f49d760
SumanK307 <sumankumarcse2026@gmail.com>
2024-12-05 23:18:02 +0530

    Add new

--- _05_ARRAY/D07_Seven.java
+++ _05_ARRAY/D07_Seven.java
@@ -25,7 +25,9 @@ public class D07_Seven {
     //**********************Find two element sum equal to target****************************/
     static int unique(int[] arr){
         int u=-1;
-    
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if(arr[i]==arr[j]){
     }
 
     public static void main(String[] args) {
