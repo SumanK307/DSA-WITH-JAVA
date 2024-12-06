8f768b81c7a6fb5d6a677c8873fe172613919da9
SumanK307 <sumankumarcse2026@gmail.com>
2024-12-06 22:26:16 +0530

    Add new

--- _05_ARRAY/D08_Eight.java
+++ _05_ARRAY/D08_Eight.java
@@ -1,5 +1,12 @@
 package _05_ARRAY;
 
+import java.util.Scanner;
+
 public class D08_Eight {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10]; 
+        System.out.print("Enter the size of array:");
+        int n=sc.nextInt();
+    }
 }
