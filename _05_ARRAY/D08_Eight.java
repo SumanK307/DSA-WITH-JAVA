24b2bb3d63746dc4bd7ed353ad555958ca69a6e2
SumanK307 <sumankumarcse2026@gmail.com>
2024-12-06 22:27:11 +0530

    Add new

--- _05_ARRAY/D08_Eight.java
+++ _05_ARRAY/D08_Eight.java
@@ -8,5 +8,15 @@ public class D08_Eight {
         int[] arr = new int[10]; 
         System.out.print("Enter the size of array:");
         int n=sc.nextInt();
+        System.out.print("Enter Array element:");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        printArray(arr,n);
+        System.out.print("Enter the index number which step do you want to rotate:");
+        int k=sc.nextInt();
+        int[] ans=Rotate(arr,k,n);           //Q.1
+
+
     }
 }
