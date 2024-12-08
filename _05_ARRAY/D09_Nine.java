17e26bf5f8554474478f495b6a70be6542674c8a
SumanK307 <sumankumarcse2026@gmail.com>
2024-12-08 21:48:01 +0530

    Add new

--- _05_ARRAY/D09_Nine.java
+++ _05_ARRAY/D09_Nine.java
@@ -1,7 +1,16 @@
 package _05_ARRAY;
 
+import java.util.Scanner;
+
 public class D09_Nine {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of array:");
+        int n=sc.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Enter Array element:");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
     }    
 }
