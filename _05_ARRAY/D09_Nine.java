c52adabed11751a8da831ffd724f43f0f46a2397
SumanK307 <sumankumarcse2026@gmail.com>
2024-12-09 23:22:21 +0530

    Add new

--- _05_ARRAY/D09_Nine.java
+++ _05_ARRAY/D09_Nine.java
@@ -10,6 +10,12 @@ public class D09_Nine {
         }
         return freq;
     }
+    static void printArray(int[] arr ,int n){
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the size of array:");
