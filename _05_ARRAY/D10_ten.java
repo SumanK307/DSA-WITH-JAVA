bbf2f6473e3f6b37108e62f0c44759e0060820fe
SumanK307 <sumankumarcse2026@gmail.com>
2024-12-10 23:44:57 +0530

    Add new

--- _05_ARRAY/D10_ten.java
+++ _05_ARRAY/D10_ten.java
@@ -2,6 +2,9 @@ package _05_ARRAY;
 import java.util.*;
 public class D10_ten {
     public static void main(String[] args) {
-        
+        Scanner var1 = new Scanner(System.in);
+      System.out.print("Enter the size of array:");
+      int var2 = var1.nextInt();
+      int[] var3 = new int[var2];
     }
 }
