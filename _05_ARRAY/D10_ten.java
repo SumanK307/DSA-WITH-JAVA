a06e2a4fba2f7887e0b0c10195ade5f7188db6d9
SumanK307 <sumankumarcse2026@gmail.com>
2024-12-10 23:46:12 +0530

    Add new

--- _05_ARRAY/D10_ten.java
+++ _05_ARRAY/D10_ten.java
@@ -6,5 +6,10 @@ public class D10_ten {
       System.out.print("Enter the size of array:");
       int var2 = var1.nextInt();
       int[] var3 = new int[var2];
+      System.out.print("Enter Array element(must contain -ve element for Q.1):");
+
+      for(int var4 = 0; var4 < var2; ++var4) {
+         var3[var4] = var1.nextInt();
+      }
     }
 }
