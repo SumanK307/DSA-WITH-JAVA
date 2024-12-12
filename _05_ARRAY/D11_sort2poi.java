46a2e88c1f50b2fd99f0baccf132cffc8db6f551
SumanK307 <sumankumarcse2026@gmail.com>
2024-12-12 14:31:01 +0530

    Add new

--- _05_ARRAY/D11_sort2poi.java
+++ _05_ARRAY/D11_sort2poi.java
@@ -3,6 +3,30 @@ package _05_ARRAY;
 import java.util.Scanner;
 
 public class D11_sort2poi {
+    static void printArray(int[] arr ,int n){
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    //************************(Q.1.1)sort 0 and 1 normal process*************************/
+    static int[] sort(int[] arr,int n){
+        int count=0;
+        for (int i = 0; i < n; i++) {
+            if(arr[i]==0){
+                count++;
+            }
+        }
+        for (int i = 0; i <n; i++) {
+            if(i<count){
+                arr[i]=0;
+            }
+            else{
+                arr[i]=1;
+            }            
+        }
+        return arr;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the size of array:");
