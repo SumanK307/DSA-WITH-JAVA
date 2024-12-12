0c9f87316bd5470af3a58cfb3e978a38c8b12f32
SumanK307 <sumankumarcse2026@gmail.com>
2024-12-12 14:29:45 +0530

    Add new

--- _05_ARRAY/D11_sort2poi.java
+++ _05_ARRAY/D11_sort2poi.java
@@ -1,7 +1,18 @@
 package _05_ARRAY;
 
+import java.util.Scanner;
+
 public class D11_sort2poi {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of array:");
+        int n=sc.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Enter Array element(only contain o and 1):");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.print("Original Array:");
+        printArray(arr,n);
     }
 }
