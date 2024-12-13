6f9ed719a301290f5e346015b3007d1905980dc8
SumanK307 <sumankumarcse2026@gmail.com>
2024-12-13 14:13:16 +0530

    Add new

--- _05_ARRAY/D11_2Sort.java
+++ _05_ARRAY/D11_2Sort.java
@@ -3,6 +3,27 @@ package _05_ARRAY;
 import java.util.Scanner;
 
 public class D11_2Sort {
+    static void swap(int[] arr, int i,int j){
+        arr[i]=arr[i]+arr[j];
+        arr[j]=arr[i]-arr[j];
+        arr[i]=arr[i]-arr[j];
+    }
+    static void sortZeroOne(int[] arr,int n){  
+        int left=0,right=n-1;
+        while(left<right){
+            if(arr[left]==1 && arr[right]==0){
+                swap(arr,left,right);
+                left++;
+                right--;
+            }
+            if(arr[left]==0){
+                left++;
+            }
+            if(arr[right]==1){
+                right--;
+            }
+        }
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the size of array:");
