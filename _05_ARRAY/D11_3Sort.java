2901486bde0822bf88515683fdc4e6a8a61385a3
SumanK307 <sumankumarcse2026@gmail.com>
2024-12-14 14:31:18 +0530

    Add new

---
+++ _05_ARRAY/D11_3Sort.java
@@ -0,0 +1,26 @@
+package _05_ARRAY;
+
+import java.util.Scanner;
+
+public class D11_3Sort {
+    static void printArray(int[] arr ,int n){
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of array:");
+        int n=sc.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Enter Array element(only contain o and 1):");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.print("Original Array:");
+        printArray(arr,n);
+        sortEvenOdd(arr,n);
+        printArray(arr,n);
+    }    
+}
