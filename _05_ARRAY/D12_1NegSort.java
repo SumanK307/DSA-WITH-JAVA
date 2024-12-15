3d69d85c917f14941e79084e5c1255b10cc73dfb
SumanK307 <sumankumarcse2026@gmail.com>
2024-12-15 23:31:00 +0530

    Add new

--- _05_ARRAY/D12_1NegSort.java
+++ _05_ARRAY/D12_1NegSort.java
@@ -3,6 +3,24 @@ package _05_ARRAY;
 import java.util.Scanner;
 
 public class D12_1NegSort {
+    static void printArray(int[] arr ,int n){
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    static void reverse(int[] arr,int i,int j){
+        while(i<j){
+            swap(arr,i,j);
+            i++;
+            j--;
+        }
+    }
+    static void swap(int[] arr, int i,int j){
+        arr[i]=arr[i]+arr[j];
+        arr[j]=arr[i]-arr[j]; 
+        arr[i]=arr[i]-arr[j];
+}
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the size of array:");
