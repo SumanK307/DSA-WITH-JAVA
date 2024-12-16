3dcfff88d353c976dc3a75e9b594c08a3d0cf184
SumanK307 <sumankumarcse2026@gmail.com>
2024-12-16 15:16:02 +0530

    Add new

--- _05_ARRAY/D12_2PrefixS.java
+++ _05_ARRAY/D12_2PrefixS.java
@@ -3,6 +3,12 @@ package _05_ARRAY;
 import java.util.Scanner;
 
 public class D12_2PrefixS {
+    static void printArray(int[] arr ,int n){
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the size of array:");
@@ -14,5 +20,7 @@ public class D12_2PrefixS {
         }
         System.out.print("Original Array:");
         printArray(arr,n);
+        int[] pre=PrefixSum(arr, n);                    //Q.2
+        printArray(pre, n);
     }    
 }
