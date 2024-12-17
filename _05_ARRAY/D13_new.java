416ffc70d950cb966ad2ce8dafe5aa58a6117636
SumanK307 <sumankumarcse2026@gmail.com>
2024-12-17 14:52:33 +0530

    Add new

--- _05_ARRAY/D13_new.java
+++ _05_ARRAY/D13_new.java
@@ -3,6 +3,18 @@ package _05_ARRAY;
 import java.util.Scanner;
 
 public class D13_new {
+    static int[] PrefixSum(int[] arr,int n){
+        for (int i = 2; i <= n; i++) {
+            arr[i]+=arr[i-1];
+        }
+        return arr;
+    }
+    static void printArray(int[] arr ,int n){
+        for (int i = 1; i <= n; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the size of array:");
