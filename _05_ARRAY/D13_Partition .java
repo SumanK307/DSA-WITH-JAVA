af9c1ad15f21c476545aa26ab9563b90f6314629
SumanK307 <sumankumarcse2026@gmail.com>
2024-12-18 14:01:01 +0530

    Add  new

--- _05_ARRAY/D13_Partition .java
+++ _05_ARRAY/D13_Partition .java
@@ -1,6 +1,12 @@
 package _05_ARRAY;
 
 public class D13_Partition{
+    static void printArray(int[] arr ,int n){
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the size of array:");
