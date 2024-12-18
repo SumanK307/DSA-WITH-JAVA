a0e26157a62cd11abacaef3ed28a278754804cf8
SumanK307 <sumankumarcse2026@gmail.com>
2024-12-18 14:00:16 +0530

    Add  new

---
+++ _05_ARRAY/D13_Partition .java
@@ -0,0 +1,20 @@
+package _05_ARRAY;
+
+public class D13_Partition{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of array:");
+        int n=sc.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Enter Array element:");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.print("Original Array:");
+        printArray(arr,n);
+        System.out.print("Sum of element in Array:");
+        int[] pre = PrefixSum(arr, n); 
+        printArray(pre,n);
+        PartitionOfArray(arr, pre, n);
+    }
+}
