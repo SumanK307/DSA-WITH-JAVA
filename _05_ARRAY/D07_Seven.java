fd82515a0f7eb3e7093de4b43fbe0a522d8b366b
SumanK307 <sumankumarcse2026@gmail.com>
2024-12-20 23:16:32 +0530

    Add new

--- _05_ARRAY/D07_Seven.java
+++ _05_ARRAY/D07_Seven.java
@@ -50,6 +50,6 @@ public class D07_Seven {
         System.out.print("Enter the sum value of two element:");
         int s=sc.nextInt();
         System.out.println("No of term which sum equl to s :"+Count(arr,s));
-        System.out.println("Unique element is:"+unique(arr));
+        //System.out.println("Unique element is:"+unique(arr));
     }
 }
