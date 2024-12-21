4e0042d41a271e82f81afc257a5805b4fa3e82b3
SumanK307 <sumankumarcse2026@gmail.com>
2024-12-21 23:47:03 +0530

    Add new

--- _05_ARRAY/D14_tut.java
+++ _05_ARRAY/D14_tut.java
@@ -3,6 +3,14 @@ package _05_ARRAY;
 import java.util.Scanner;
 
 public class D14_tut {
+    static void printArray(int[][] arr){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         System.out.print("Enter the size of Row:");
