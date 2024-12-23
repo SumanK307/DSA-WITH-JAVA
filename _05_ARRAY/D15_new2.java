9381118eea59c9adf36572747c5c297e99b82e1e
SumanK307 <sumankumarcse2026@gmail.com>
2024-12-23 15:00:10 +0530

    Add new

--- _05_ARRAY/D15_new2.java
+++ _05_ARRAY/D15_new2.java
@@ -1,5 +1,7 @@
 package _05_ARRAY;
 
+import java.util.Scanner;
+
 public class D15_new2 {
     static void printArray(int[][] arr){
         for (int i = 0; i < arr.length; i++) {
@@ -34,6 +36,19 @@ public class D15_new2 {
             }
         }
     public static void main(String[] args) {
-        
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the size of Row:");
+        int r=sc.nextInt();
+        System.out.print("Enter the size of colummn:");
+        int c=sc.nextInt();
+        System.out.println("Enter Array Element:");
+        int[][] arr = new int[r][c];
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        System.out.println("Original Array:");
+        printArray(arr);
     }
 }
