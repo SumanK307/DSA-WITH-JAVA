c7d355cc551668a86c2027a4ffa6e5e4d4f1c6b2
SumanK307 <sumankumarcse2026@gmail.com>
2024-12-25 23:30:45 +0530

    Add new

---
+++ _05_ARRAY/D16_Passcal.java
@@ -0,0 +1,18 @@
+package _05_ARRAY;
+
+import java.util.Scanner;
+
+public class D16_Passcal {
+    static void printArray(int[][] arr){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
