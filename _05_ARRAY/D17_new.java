a6f9028d37fc382a5e73060a959a8d3df80794ce
SumanK307 <sumankumarcse2026@gmail.com>
2024-12-26 13:32:26 +0530

    Add new

---
+++ _05_ARRAY/D17_new.java
@@ -0,0 +1,16 @@
+package _05_ARRAY;
+
+import java.util.Scanner;
+
+public class D17_new {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of Row:");
+        int r = sc.nextInt();
+        System.out.print("Enter the size of colummn:");
+        int c = sc.nextInt();
+        System.out.println("Enter Array Element:");
+        int[][] arr = new int[r][c];
+        int[][] arr2 = new int[r][c];
+    }
+}
