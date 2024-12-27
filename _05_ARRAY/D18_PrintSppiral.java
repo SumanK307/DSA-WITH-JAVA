e0a3dcfff4c3abdb2e5ff3e80c55ebd0a813ffd0
SumanK307 <sumankumarcse2026@gmail.com>
2024-12-27 19:13:49 +0530

    Add new

--- _05_ARRAY/D18_PrintSppiral.java
+++ _05_ARRAY/D18_PrintSppiral.java
@@ -1,5 +1,6 @@
 package _05_ARRAY;
 
+import java.util.Scanner;
 
 public class D18_PrintSppiral {
    // Print in spiral order
@@ -42,7 +43,27 @@ public class D18_PrintSppiral {
         }
         return arr2;
     }      
+    static void printArray(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of Row:");
+        int r = sc.nextInt();
+        System.out.print("Enter the size of colummn:");
+        int c = sc.nextInt();
+        System.out.println("Enter Array Element:");
+        int[][] arr = new int[r][c];
+        int[][] arr2 = new int[r][c];
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
     }
 }
