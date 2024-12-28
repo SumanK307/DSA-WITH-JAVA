6286f492efc5627da67d52f369cdb41c4c0d3bf7
SumanK307 <sumankumarcse2026@gmail.com>
2024-12-28 18:07:41 +0530

    Add new

---
+++ _05_ARRAY/D19_SuminMatrix.java
@@ -0,0 +1,23 @@
+package _05_ARRAY;
+
+import java.util.Scanner;
+
+public class D19_SuminMatrix {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of Row:");
+        int r = sc.nextInt();
+        System.out.print("Enter the size of colummn:");
+        int c = sc.nextInt();
+        System.out.println("Enter Array Element:");
+        int[][] arr = new int[r][c];
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Original Array:");
+        printArray(arr);
+
+    }
+}
