651ce0f44e694ed8e96081d18c293111aa71517f
SumanK307 <sumankumarcse2026@gmail.com>
2024-12-28 18:08:17 +0530

    Add new

--- _05_ARRAY/D19_SuminMatrix.java
+++ _05_ARRAY/D19_SuminMatrix.java
@@ -3,6 +3,27 @@ package _05_ARRAY;
 import java.util.Scanner;
 
 public class D19_SuminMatrix {
+    static void printArray(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    static void findPrefixSum(int[][] arr){
+        int r=arr.length;
+        int c=arr[0].length;
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                arr[i][j] += arr[i][j-1];
+            }
+        }
+        for (int j = 0; j < c; j++) {
+            for (int i = 0; i < r; i++) {
+                arr[i][j] += arr[i-1][j];
+            }
+        
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
         System.out.print("Enter the size of Row:");
