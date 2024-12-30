52cbc7bca3c93099b6b0af48715d0f772f857437
SumanK307 <sumankumarcse2026@gmail.com>
2024-12-30 20:24:38 +0530

    Add new

--- STACK/D01_Start.java
+++ STACK/D01_Start.java
@@ -1,7 +1,12 @@
 package STACK;
 
+import java.util.Stack;
+
 public class D01_Start {
     public static void main(String[] args) {
-        
+         Stack<Integer> st = new Stack<>(); 
+         st.push(3);
+         st.push(8);
+         st.push(2);
     }
 }
