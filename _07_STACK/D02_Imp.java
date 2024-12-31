17461f811d8b11a8936fe1b5f593055b470b82f6
SumanK307 <sumankumarcse2026@gmail.com>
2025-01-01 01:28:54 +0530

    Add new

--- _07_STACK/D02_Imp.java
+++ _07_STACK/D02_Imp.java
@@ -9,7 +9,7 @@ public class D02_Imp {
          Stack<Integer> st = new Stack<>();
          System.out.print("Enter the no of element:");
          int n = sc.nextInt(); 
-         System.out.print("traverse");
+         System.out.print("traverse in stack");
          // normal order
          for (int i = 1; i <= n; i++) {
             int ele = sc.nextInt();
