1f9b52f3d1c0e932f38664fc5bcfe853d21472fb
SumanK307 <sumankumarcse2026@gmail.com>
2024-12-31 15:32:25 +0530

    Add new

--- _07_STACK/D02_Imp.java
+++ _07_STACK/D02_Imp.java
@@ -1,7 +1,19 @@
 package _07_STACK;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class D02_Imp {
     public static void main(String[] args) {
-        
+         Scanner sc = new Scanner(System.in);
+         Stack<Integer> st = new Stack<>();
+         System.out.print("Enter the no of element:");
+         int n = sc.nextInt(); 
+         // normal order
+         for (int i = 1; i <= n; i++) {
+            int ele = sc.nextInt();
+            st.push(ele);
+         }
+         System.out.println(st);
     }
 }
