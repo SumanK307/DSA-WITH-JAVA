0dd49353ab320ad4590121e104678b14d123c972
SumanK307 <sumankumarcse2026@gmail.com>
2025-01-02 15:48:46 +0530

    Add new

---
+++ _07_STACK/D03_new.java
@@ -0,0 +1,16 @@
+package _07_STACK;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class D03_new {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+         Stack<Integer> st = new Stack<>();
+         st.push(3);
+         st.push(8);
+         st.push(2);
+         st.push(6);         
+         System.out.println(st);
+    }    
+}
