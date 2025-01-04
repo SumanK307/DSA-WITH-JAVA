012430c4e02a1206e2926d76e975cab22352d31b
SumanK307 <sumankumarcse2026@gmail.com>
2025-01-04 15:50:24 +0530

    Add new

---
+++ _07_STACK/D05_ArrayImp.java
@@ -0,0 +1,17 @@
+package _07_STACK;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class D05_ArrayImp {
+    public static void main(String[] args){
+         Scanner sc = new Scanner(System.in);
+         Stack<Integer> st = new Stack<>();
+         st.push(3);
+         st.push(8);
+         st.push(2);
+         st.push(6);         
+         System.out.println(st);
+         
+    }
+}
