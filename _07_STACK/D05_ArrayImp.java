c928affe471f17d9f52d42595c1cfb98d9f58672
SumanK307 <sumankumarcse2026@gmail.com>
2025-01-04 15:51:20 +0530

    Add new

--- _07_STACK/D05_ArrayImp.java
+++ _07_STACK/D05_ArrayImp.java
@@ -4,6 +4,15 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class D05_ArrayImp {
+    public static void displayRec(Stack<Integer> st){ //pass by refrence
+        if (st.size() == 0) 
+            return;
+        int tem = st.pop();
+        System.out.print(tem+" ");                    //for reverse stack  
+        displayRec(st);                               
+        System.out.print(tem+" ");                                           
+        //st.push(tem);
+    }
     public static void main(String[] args){
          Scanner sc = new Scanner(System.in);
          Stack<Integer> st = new Stack<>();
@@ -22,6 +31,6 @@ public class D05_ArrayImp {
         //     System.out.print(arr[i]+" ");
         //     st.push(arr[i]);
         // }
-        
+
     }
 }
