1959a3b027a7815b4c0e49164880986196b86011
SumanK307 <sumankumarcse2026@gmail.com>
2025-01-07 13:40:19 +0530

    Add new

--- _07_STACK/D08_new.java
+++ _07_STACK/D08_new.java
@@ -3,6 +3,21 @@ package _07_STACK;
 import java.util.*;
 
 public class D08_new {
+
+    public static void reverse(Stack<Integer> st){ 
+        if (st.size() == 0) 
+            return;
+        int tem = st.pop();
+        reverse(st);                          
+        st.push(tem);
+    }
+    public static void reverseStack(Stack<Integer> st){ 
+        if (st.size() == 1) 
+            return;
+        int tem = st.pop();
+        reverse(st);
+        InsertAtBottom(st,tem);
+    }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
          Stack<Integer> st = new Stack<>();
