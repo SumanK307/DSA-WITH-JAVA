82c07a502b012266c716bb974c873ec744296c5b
SumanK307 <sumankumarcse2026@gmail.com>
2025-01-08 18:01:03 +0530

    Add new

--- _07_STACK/D09_Question.java
+++ _07_STACK/D09_Question.java
@@ -3,6 +3,14 @@ package _07_STACK;
 import java.util.Scanner;
 
 public class D09_Question {
+    public static boolean isBalance(String str){
+        Stack<Character> st = new Stack<>();
+        int n = str.length();
+        for (int i = 0; i < n; i++) {
+            char ch = str.charAt(i);
+            if(ch == '('){
+                st.push(ch);
+            }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter any String:");
