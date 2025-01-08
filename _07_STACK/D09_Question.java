7218b845efd5442975b2cda1a2575699dd2e971b
SumanK307 <sumankumarcse2026@gmail.com>
2025-01-08 18:00:27 +0530

    Add new

---
+++ _07_STACK/D09_Question.java
@@ -0,0 +1,12 @@
+package _07_STACK;
+
+import java.util.Scanner;
+
+public class D09_Question {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any String:");
+        String str = sc.nextLine();
+        System.out.println(str);
+    }
+}
