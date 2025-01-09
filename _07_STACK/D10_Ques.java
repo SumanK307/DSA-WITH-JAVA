b5a4a3e673ef06a88034973add5661d210d90905
SumanK307 <sumankumarcse2026@gmail.com>
2025-01-09 13:12:27 +0530

    Add new

---
+++ _07_STACK/D10_Ques.java
@@ -0,0 +1,16 @@
+package _07_STACK;
+
+import java.util.Scanner;
+
+public class D10_Ques {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+            System.out.print("Enter Number:");
+            int n = sc.nextInt();
+            int[] arr = new int[n]; 
+            for(int i = 0; i < n; i++){
+                arr[i] = sc.nextInt();
+            }
+            
+    }
+}
