a7bb8e0d08b9704afad91f370f9056422aa7ee88
SumanK307 <sumankumarcse2026@gmail.com>
2025-01-09 13:13:21 +0530

    Add new

--- _07_STACK/D10_Ques.java
+++ _07_STACK/D10_Ques.java
@@ -1,8 +1,17 @@
 package _07_STACK;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class D10_Ques {
+    public static void Print(int[] arr,int n){
+        for(int i = 0; i < n; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    } 
+    public static void removeAllDuplicate(int[] arr,int n){
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < n; i++) {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
             System.out.print("Enter Number:");
@@ -11,6 +20,6 @@ public class D10_Ques {
             for(int i = 0; i < n; i++){
                 arr[i] = sc.nextInt();
             }
-            
+
     }
 }
