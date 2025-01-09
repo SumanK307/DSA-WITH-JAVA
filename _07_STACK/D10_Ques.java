032b973c76f0715f9df293ec1fbd3141ee9a6294
SumanK307 <sumankumarcse2026@gmail.com>
2025-01-09 13:13:43 +0530

    Add new

--- _07_STACK/D10_Ques.java
+++ _07_STACK/D10_Ques.java
@@ -12,6 +12,22 @@ public class D10_Ques {
     public static void removeAllDuplicate(int[] arr,int n){
         Stack<Integer> st = new Stack<>();
         for (int i = 0; i < n; i++) {
+            if(st.size() == 0 || st.peek() != arr[i]){
+                st.push(arr[i]);
+            }
+            else if(arr[i]==st.peek() ){
+                if(i==n-1 || arr[i]!=arr[i+1])
+                    st.pop();
+            }
+        }
+        // int[] res = new int[st.size()];
+        // int m = res.length;
+        // for (int i = m-1; i >= 0; i--) {
+        //     res[i] = st.pop();
+        // }
+        // return res;
+        System.out.println(st);
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
             System.out.print("Enter Number:");
