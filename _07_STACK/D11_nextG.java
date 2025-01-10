cf9d9f988205b55ee8430b1109e2e008674d2d78
SumanK307 <sumankumarcse2026@gmail.com>
2025-01-10 23:11:17 +0530

    Add new

--- _07_STACK/D11_nextG.java
+++ _07_STACK/D11_nextG.java
@@ -3,6 +3,18 @@ package _07_STACK;
 import java.util.Scanner;
 
 public class D11_nextG {
+    // Q.1 print next greater element in array using Stack
+//      [4,7,3,9,6,8] -> [7,9,9,-1,8,-1]
+// Q.2 2nd Approach
+// Q.3 previous greater 
+public class _10_NextGreater {
+    public static void Print(int[] arr,int n){
+        for(int i = 0; i < n; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    } 
+    
     public static void main(String[] args) {
          Scanner sc = new Scanner(System.in);
             System.out.print("Enter Number:");
