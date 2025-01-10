4b4ba2e6ccabd9006ce47c006b5d5247b25ee8ff
SumanK307 <sumankumarcse2026@gmail.com>
2025-01-10 23:13:54 +0530

    Add new

--- _07_STACK/D11_nextG.java
+++ _07_STACK/D11_nextG.java
@@ -1,13 +1,12 @@
 package _07_STACK;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class D11_nextG {
     // Q.1 print next greater element in array using Stack
 //      [4,7,3,9,6,8] -> [7,9,9,-1,8,-1]
 // Q.2 2nd Approach
 // Q.3 previous greater 
-public class _10_NextGreater {
     public static void Print(int[] arr,int n){
         for(int i = 0; i < n; i++){
             System.out.print(arr[i]+" ");
@@ -28,6 +27,24 @@ public class _10_NextGreater {
             else
                 res[i] = st.peek();
             st.push(arr[i]);
+        }
+        return res;
+    }
+    //Second Approach
+    public static int[] nextGreater2(int[] arr,int n){
+        Stack<Integer> st = new Stack<>();
+        int[] res = new int[n];
+        res[n-1] = -1;
+        for (int i = 0; i < n-1 ; i++) {
+            st.push(i);
+                while(arr[i]<arr[i+1] && !st.isEmpty()){
+                res[st.peek()]=arr[i+1];
+                st.pop();
+            }
+        }
+        while (!st.isEmpty()) {
+            res[st.peek()]=-1;
+            st.pop();
         }
         return res;
     }
