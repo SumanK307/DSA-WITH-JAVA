65feaae5225185d0e1962a4a2f7992cddc18633e
SumanK307 <sumankumarcse2026@gmail.com>
2025-01-11 13:39:24 +0530

    Add new

--- _07_STACK/D12_new.java
+++ _07_STACK/D12_new.java
@@ -3,6 +3,18 @@ package _07_STACK;
 import java.util.*;
 
 public class D12_new {
+    public static void Print(int[] arr, int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static int[] stockSpan(int[] arr, int n) {
+        Stack<Integer> st = new Stack<>();
+        int[] res = new int[n];
+        res[0] = 1;
+        st.push(0);
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Number:");
