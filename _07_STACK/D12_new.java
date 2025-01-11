37034e9a053b4b335edb19eed911d0ddae377209
SumanK307 <sumankumarcse2026@gmail.com>
2025-01-11 13:38:40 +0530

    Add new

---
+++ _07_STACK/D12_new.java
@@ -0,0 +1,15 @@
+package _07_STACK;
+
+import java.util.*;
+
+public class D12_new {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number:");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+    }
+}
