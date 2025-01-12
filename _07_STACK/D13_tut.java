412daf76f2481c5c2ee3645e3385010269db4843
SumanK307 <sumankumarcse2026@gmail.com>
2025-01-12 15:03:29 +0530

    Add new

--- _07_STACK/D13_tut.java
+++ _07_STACK/D13_tut.java
@@ -1,6 +1,6 @@
 package _07_STACK;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class D13_tut {
     public static void main(String[] args) {
@@ -12,5 +12,21 @@ public class D13_tut {
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
             int ascii = (int)ch;
+            if(ascii >= 48 && ascii <= 57)  val.push(ascii-48);
+            else if(op.size()==0 || ch == '(' || op.peek() == '(')   op.push(ch);
+            else if(ch == ')'){
+                while(op.peek()!='('){
+                    int v2 =val.pop();
+                    int v1 =val.pop();
+                    if(op.peek() == '+')    val.push(v1+v2);
+                    if(op.peek() == '-')    val.push(v1-v2);
+                    if(op.peek() == '*')    val.push(v1*v2);
+                    if(op.peek() == '/')    val.push(v1/v2);
+                    op.pop();
+                }
+                op.pop();
+            }
+            else{
+                
     }
 }
