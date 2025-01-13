97caaeca4b87c36063e15b3f9353cc5d8d31b05f
SumanK307 <sumankumarcse2026@gmail.com>
2025-01-13 14:06:19 +0530

    Add new

---
+++ _07_STACK/D14_tut.java
@@ -0,0 +1,18 @@
+package _07_STACK;
+import java.util.*;
+
+public class D14_tut {
+    public static void main(String[] args) {
+        //*****************Infix to prefix*****************//
+//         String infix = "8-(4+6)*4/8"; 
+//         System.out.println(infix);
+//         Stack<String> val = new Stack<>();
+//         Stack<Character> op = new Stack<>();
+//         for (int i = 0; i < infix.length(); i++) {
+//             char ch = infix.charAt(i);
+//             int ascii = (int)ch;
+//             if(ascii >= 48 && ascii <= 57){  String s = "" + ch;
+//             val.push(s);
+//         }
+    }
+}
