9183fdb44d44d2d6b7637d99d7eeee4de68ead57
SumanK307 <sumankumarcse2026@gmail.com>
2025-01-14 22:17:47 +0530

    Add new

---
+++ _07_STACK/D15_new.java
@@ -0,0 +1,17 @@
+package _07_STACK;
+
+public class D15_new {
+    public static void main(String[] args) {
+        //**************Infix to postfix***************//
+        String infix = "8-(4+6)*4/8"; 
+        System.out.println(infix);
+        Stack<String> val = new Stack<>();
+        Stack<Character> op = new Stack<>();
+        for (int i = 0; i < infix.length(); i++) {
+            char ch = infix.charAt(i);
+            int ascii = (int)ch;
+            if(ascii >= 48 && ascii <= 57){  String s = "" + ch;
+            val.push(s);
+        }
+    }
+}
