bf685e0d3ec40f93c3c9d7c1033abd125352bbc4
SumanK307 <sumankumarcse2026@gmail.com>
2025-01-14 22:18:35 +0530

    Add new

--- _07_STACK/D15_new.java
+++ _07_STACK/D15_new.java
@@ -13,5 +13,25 @@ public class D15_new {
             if(ascii >= 48 && ascii <= 57){  String s = "" + ch;
             val.push(s);
         }
+        else if(op.size()==0 || ch == '(' || op.peek() == '(')   op.push(ch);
+            else if(ch == ')'){
+                while(op.peek()!='('){
+                    String v2 =val.pop();
+                    String v1 =val.pop();
+                    char o = op.pop();
+                    String t = v1+v2+o;
+                    val.push(t);
+                }
+                op.pop();
+            }
+            else{
+                if(ch == '+' || ch == '-'){
+                    String v2 =val.pop();
+                    String v1 =val.pop();
+                    char o = op.pop();
+                    String t = v1+v2+o;
+                    val.push(t);
+                    op.push(ch);
+                }
     }
 }
