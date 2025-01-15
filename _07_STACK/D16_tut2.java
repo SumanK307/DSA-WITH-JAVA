a77d8651cd3fca42ea157196011f6dd959b51804
SumanK307 <sumankumarcse2026@gmail.com>
2025-01-15 13:59:29 +0530

    Add new

--- _07_STACK/D16_tut2.java
+++ _07_STACK/D16_tut2.java
@@ -1,27 +1,28 @@
 package _07_STACK;
+import java.util.*;
 
 public class D16_tut2 {
     public static void main(String[] args){
         //**************Evaluation of postfix**************//
         
-        //         String postfix = "953+4*6/-";
-        //         Stack<Integer> val = new Stack<>();
-        //         for (int i = 0; i < postfix.length(); i++) {
-        //             char ch = postfix.charAt(i);
-        //             int ascii = (int)ch;
+                String postfix = "953+4*6/-";
+                Stack<Integer> val = new Stack<>();
+                for (int i = 0; i < postfix.length(); i++) {
+                    char ch = postfix.charAt(i);
+                    int ascii = (int)ch;
         if(ascii>=48 && ascii<=57){
-            //                 val.push(ascii-48);
-            //             }
-            //             else{
-            //                 int v2 = val.pop();
-            //                 int v1 = val.pop();
-            //                 if(ch == '+')   val.push(v1+v2);
-            //                 if(ch == '-')   val.push(v1-v2);
-            //                 if(ch == '*')   val.push(v1*v2);
-            //                 if(ch == '/')   val.push(v1/v2);
-            //             }
-        //}
-        //         int ans = val.pop();
-        //         System.out.println(ans);
-        //     } 
+                            val.push(ascii-48);
+                        }
+                        else{
+                            int v2 = val.pop();
+                            int v1 = val.pop();
+                            if(ch == '+')   val.push(v1+v2);
+                            if(ch == '-')   val.push(v1-v2);
+                            if(ch == '*')   val.push(v1*v2);
+                            if(ch == '/')   val.push(v1/v2);
+                        }
+        }
+                int ans = val.pop();
+                System.out.println(ans);
+            } 
 }
