7869c1e76efca80c95630c1f045e91ba7029b31d
SumanK307 <sumankumarcse2026@gmail.com>
2025-01-15 13:58:06 +0530

    Add new

---
+++ _07_STACK/D16_tut2.java
@@ -0,0 +1,12 @@
+package _07_STACK;
+
+public class D16_tut2 {
+    public static void main(String[] args){
+        //**************Evaluation of postfix**************//
+        
+        //         String postfix = "953+4*6/-";
+        //         Stack<Integer> val = new Stack<>();
+        //         for (int i = 0; i < postfix.length(); i++) {
+        //             char ch = postfix.charAt(i);
+        //             int ascii = (int)ch;
+}
