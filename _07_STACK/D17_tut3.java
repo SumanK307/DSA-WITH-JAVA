fa977ebfa003d26b64f57d19c17f15275adea601
SumanK307 <sumankumarcse2026@gmail.com>
2025-01-16 21:53:49 +0530

    Add new

--- _07_STACK/D17_tut3.java
+++ _07_STACK/D17_tut3.java
@@ -3,6 +3,12 @@ package _07_STACK;
 
 public class D17_tut3 {
     public static void main(String[] args) {
-        
+        //**************Evaluation of prefix**************//
+
+String prefix = "-+7*45+20";
+Stack<Integer> val = new Stack<>();
+for (int i = prefix.length()-1; i >= 0; i--) {
+    char ch = prefix.charAt(i);
+    int ascii = (int)ch;
     }
 }
