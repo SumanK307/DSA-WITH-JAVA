43add44fa79600f690ea44a3cbbaa6c7c18790e0
SumanK307 <sumankumarcse2026@gmail.com>
2025-01-16 21:54:32 +0530

    Add new

--- _07_STACK/D17_tut3.java
+++ _07_STACK/D17_tut3.java
@@ -1,5 +1,5 @@
 package _07_STACK;
-
+import java.util.*;
 
 public class D17_tut3 {
     public static void main(String[] args) {
@@ -10,5 +10,8 @@ Stack<Integer> val = new Stack<>();
 for (int i = prefix.length()-1; i >= 0; i--) {
     char ch = prefix.charAt(i);
     int ascii = (int)ch;
+    }
+    if(ascii>=48 && ascii<=57){
+        val.push(ascii-48);
     }
 }
