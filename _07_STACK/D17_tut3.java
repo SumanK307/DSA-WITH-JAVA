89a7a9701a9ff197b6bc3090cbc5d794447f6744
SumanK307 <sumankumarcse2026@gmail.com>
2025-01-16 21:55:07 +0530

    Add new

--- _07_STACK/D17_tut3.java
+++ _07_STACK/D17_tut3.java
@@ -1,17 +1,32 @@
 package _07_STACK;
+
 import java.util.*;
 
 public class D17_tut3 {
     public static void main(String[] args) {
-        //**************Evaluation of prefix**************//
+        // **************Evaluation of prefix**************//
 
-String prefix = "-+7*45+20";
-Stack<Integer> val = new Stack<>();
-for (int i = prefix.length()-1; i >= 0; i--) {
-    char ch = prefix.charAt(i);
-    int ascii = (int)ch;
-    }
-    if(ascii>=48 && ascii<=57){
-        val.push(ascii-48);
+        String prefix = "-+7*45+20";
+        Stack<Integer> val = new Stack<>();
+        for (int i = prefix.length() - 1; i >= 0; i--) {
+            char ch = prefix.charAt(i);
+            int ascii = (int) ch;
+            if (ascii >= 48 && ascii <= 57) {
+                val.push(ascii - 48);
+            } else {
+                int v1 = val.pop();
+                int v2 = val.pop();
+                if (ch == '+')
+                    val.push(v1 + v2);
+                if (ch == '-')
+                    val.push(v1 - v2);
+                if (ch == '*')
+                    val.push(v1 * v2);
+                if (ch == '/')
+                    val.push(v1 / v2);
+            }
+        }
+        int ans = val.pop();
+        System.out.println(ans);
     }
 }
