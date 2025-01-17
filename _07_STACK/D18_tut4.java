c2b0415ddf9fa7e650c8027b096f488dd628e3b5
SumanK307 <sumankumarcse2026@gmail.com>
2025-01-17 23:28:03 +0530

    Add new

---
+++ _07_STACK/D18_tut4.java
@@ -0,0 +1,9 @@
+package _07_STACK;
+
+
+public class D18_tut4 {
+    public static void main(String[] args) {
+        // *****************convert prefix to postfix*****************//
+//         String prefix = "-+7*45+20";
+//         System.out.println("prefix is:" + prefix);
+}
