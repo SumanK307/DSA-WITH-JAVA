d54129067be5dde069505cc609b95a75ce7e311b
SumanK307 <sumankumarcse2026@gmail.com>
2025-01-17 23:29:08 +0530

    Add new

--- _07_STACK/D18_tut4.java
+++ _07_STACK/D18_tut4.java
@@ -6,4 +6,8 @@ public class D18_tut4 {
         // *****************convert prefix to postfix*****************//
 //         String prefix = "-+7*45+20";
 //         System.out.println("prefix is:" + prefix);
+//Stack<String> val = new Stack<>();
+//         for (int i = prefix.length()-1; i >= 0; i--){
+//             char ch = prefix.charAt(i);
+//             int ascii = (int)ch;
 }
