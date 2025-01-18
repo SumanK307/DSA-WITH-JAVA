ffd04bf61c35f80bb825fd60ab086cbba921a6d1
SumanK307 <sumankumarcse2026@gmail.com>
2025-01-18 22:01:21 +0530

    Add new

--- _07_STACK/D19_tut5.java
+++ _07_STACK/D19_tut5.java
@@ -8,5 +8,9 @@ public class D19_tut5 {
         for (int i = 0; i < postfix.length(); i++){ 
             char ch = postfix.charAt(i);
             int ascii = (int)ch;
+            if (ascii >= 48 && ascii <= 57) {
+                String s = "" + ch;
+                val.push(s);
+            } 
     }
 }
