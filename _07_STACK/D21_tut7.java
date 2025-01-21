d41eab30e13e2e32f5b31c0f2fe983431abd205a
SumanK307 <sumankumarcse2026@gmail.com>
2025-01-21 23:04:45 +0530

    Add new

--- _07_STACK/D21_tut7.java
+++ _07_STACK/D21_tut7.java
@@ -8,5 +8,9 @@ public class D21_tut7 {
 //         for (int i = 0; i < postfix.length(); i++){ 
 //             char ch = postfix.charAt(i);
 //             int ascii = (int)ch;
+//    if (ascii >= 48 && ascii <= 57) {
+    //                 String s = "" + ch;
+    //                 val.push(s);
+    //             } 
     }
 }
