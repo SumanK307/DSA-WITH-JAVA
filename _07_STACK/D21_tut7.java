26e138bfb7948b37c822b45bf92265a558ec4d7c
SumanK307 <sumankumarcse2026@gmail.com>
2025-01-21 23:03:33 +0530

    Add new

---
+++ _07_STACK/D21_tut7.java
@@ -0,0 +1,12 @@
+package _07_STACK;
+
+public class D21_tut7 {
+    public static void main(String[] args) {
+        // String postfix = "745*+27+-";
+//         System.out.println("postfix is:" + postfix);
+//         Stack<String> val = new Stack<>();
+//         for (int i = 0; i < postfix.length(); i++){ 
+//             char ch = postfix.charAt(i);
+//             int ascii = (int)ch;
+    }
+}
