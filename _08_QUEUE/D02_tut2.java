6c5fa1bb973a091c317b01fffe1c6bcf8faf35bb
SumanK307 <sumankumarcse2026@gmail.com>
2025-01-23 20:51:55 +0530

    Add new

--- _08_QUEUE/D02_tut2.java
+++ _08_QUEUE/D02_tut2.java
@@ -2,6 +2,25 @@ package _08_QUEUE;
 import java.util.*;
 
 public class D02_tut2 {
+    public static class ArrQueue{
+        private int[] arr = new int[7];
+        private int size = 0;
+        private int rear = -1;
+        private int front = -1;
+        void add(int x){
+            if(isFull()){
+                System.out.println("Queue is full.");
+                return; 
+            }
+            if(size == 0){   
+                arr[++front] = x;
+                rear++; 
+                size++;
+                return;
+            }
+            arr[++rear] = x;
+            size++;
+        }
 
     public static void main(String[] args) {
         ArrQueue aq = new ArrQueue();
