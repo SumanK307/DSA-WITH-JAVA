4da3ae7a77e3d2198a4405e5e6e3fd7960b5dc59
SumanK307 <sumankumarcse2026@gmail.com>
2025-01-25 23:25:24 +0530

    dd new

--- _08_QUEUE/D03_tut3.java
+++ _08_QUEUE/D03_tut3.java
@@ -1,6 +1,31 @@
 package _08_QUEUE;
 
 public class D03_tut3 {
+    public static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data = data;
+        }
+    }
+    public static class LL_Queue{
+    private Node front = null;
+    private Node rear = null;
+    private int size = 0;
+        void add(int data){
+            Node temp = new Node(data);
+            if(isEmpty()){
+                //head = temp;
+                front = temp;
+                rear = temp;
+                size++;
+                return;
+            }
+            rear.next = temp;
+            rear = temp; 
+            size++; 
+        }
+        
     public static void main(String[] args) {
         LL_Queue lq = new LL_Queue();
         lq.add(4);
