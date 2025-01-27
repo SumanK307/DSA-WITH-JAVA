001f425579107c4946d7ff396311ac1fdf3078b5
SumanK307 <sumankumarcse2026@gmail.com>
2025-01-27 23:35:24 +0530

    Add new

--- _08_QUEUE/D05_tut5.java
+++ _08_QUEUE/D05_tut5.java
@@ -1,11 +1,31 @@
 package _08_QUEUE;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.*;
 
 public class D05_tut5{
-        
+
     public static void main(String[] args) {
+        public static class Queue{
+        Stack<Integer> s1 = new Stack<>();
+        Stack<Integer> s2 = new Stack<>();
+        private int size = 0;
+        boolean isEmpty(){
+            return s1.isEmpty();      //if s1 empty then queue empty
+        }
+        void add(int val){
+            if(s1.size() == 0){
+                s1.push(val);
+                size++;
+                return;
+            }
+            while (s1.size() != 0) {
+                s2.push(s1.pop());
+            }
+            s1.push(val);
+            while (s2.size() != 0) {
+                s1.push(s2.pop());
+            }
+            size++;
+        }
         
     }
 }
