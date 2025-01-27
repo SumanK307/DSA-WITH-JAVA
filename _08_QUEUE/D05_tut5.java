ddf518863925ee01c1507d7decf72c8f39eca55c
SumanK307 <sumankumarcse2026@gmail.com>
2025-01-27 23:34:47 +0530

    Add new

---
+++ _08_QUEUE/D05_tut5.java
@@ -0,0 +1,11 @@
+package _08_QUEUE;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.*;
+
+public class D05_tut5{
+        
+    public static void main(String[] args) {
+        
+    }
+}
