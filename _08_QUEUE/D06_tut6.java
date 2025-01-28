749c2e2effbd4d06ffd69c66fe0ceff3f904799e
SumanK307 <sumankumarcse2026@gmail.com>
2025-01-28 23:43:03 +0530

    Add new

--- _08_QUEUE/D06_tut6.java
+++ _08_QUEUE/D06_tut6.java
@@ -23,6 +23,27 @@ public class D06_tut6{
                 System.out.println("Stack is empty.");
                 return -1; 
             }
+            int top = -1;
+            if(!q1.isEmpty()){
+                while (!q1.isEmpty()) {
+                    top = q1.remove();
+                    if (q1.isEmpty()) {
+                        break;
+                    }
+                    q2.add(top);
+                }
+            }
+            else{
+                while (!q2.isEmpty()) {
+                    top = q2.remove();
+                    if (q2.isEmpty()) {
+                        break;
+                    }
+                    q1.add(top);
+            }
+        }
+        return top;
+    }
     public static void main(String args[]){
         Stack s = new Stack();
         s.push(5);
