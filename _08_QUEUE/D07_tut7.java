1b6702e957240cbfb1aacc415eaba91532ad9ff8
SumanK307 <sumankumarcse2026@gmail.com>
2025-01-29 22:33:07 +0530

    Add new

--- _08_QUEUE/D07_tut7.java
+++ _08_QUEUE/D07_tut7.java
@@ -1,8 +1,12 @@
 package _08_QUEUE;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class D07_tut7 {
+    public static int[] kWindow(int[] arr,int k){
+
+        return arr;
+    }
     public static void main(String[] args) {
         // (Q.1) First negative in each k window
         Scanner sc = new Scanner(System.in);
