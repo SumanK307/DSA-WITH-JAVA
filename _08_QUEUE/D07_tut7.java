c479793a425730971bee3a63f0918f1794c2112d
SumanK307 <sumankumarcse2026@gmail.com>
2025-01-29 22:32:25 +0530

    Add new

---
+++ _08_QUEUE/D07_tut7.java
@@ -0,0 +1,11 @@
+package _08_QUEUE;
+
+import java.util.Scanner;
+
+public class D07_tut7 {
+    public static void main(String[] args) {
+        // (Q.1) First negative in each k window
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+    }
+}
