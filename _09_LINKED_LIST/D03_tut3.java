a3ebc862a90b3e7b0dacbd44a9025b3250eed8d6
SumanK307 <sumankumarcse2026@gmail.com>
2025-02-01 00:04:45 +0530

    Add new

--- _09_LINKED_LIST/D03_tut3.java
+++ _09_LINKED_LIST/D03_tut3.java
@@ -12,6 +12,25 @@ public class D03_tut3 {
     public static class LinkedList{
         Node head = null;
         Node tail = null;
+        void InsertAtHead(int val){
+            Node temp = new Node(val);
+            if(head == null){           // for empty list
+                head = temp;
+                tail = temp;
+            }
+            else{                       // for available list
+                temp.next = head;       // present tail ko temp se connect karega
+                head = temp;            // tail new value ko bana dega
+            }
+        }
+        void Display(){
+            Node temp = head;
+            while (temp != null) {
+                System.out.print(temp.data+" ");
+                temp = temp.next;
+            }
+        }
+    }
         
     public static void main(String[] args) {
         LinkedList li = new LinkedList();
