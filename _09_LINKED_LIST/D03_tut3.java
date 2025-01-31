3f78a36ea02328f91762ebf7a059667ca31cd41b
SumanK307 <sumankumarcse2026@gmail.com>
2025-02-01 00:03:06 +0530

    Add new

---
+++ _09_LINKED_LIST/D03_tut3.java
@@ -0,0 +1,17 @@
+package _09_LINKED_LIST;
+
+public class D03_tut3 {
+    public static void main(String[] args) {
+        public static class Node{
+            int data;   
+            Node next;
+            Node(int data){
+                this.data=data;
+            }
+        }
+    
+        public static class LinkedList{
+            Node head = null;
+            Node tail = null;
+    }    
+}
