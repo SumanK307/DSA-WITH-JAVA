4f1f0df7f7a9e628d34d51025c1894ac290f4ce0
SumanK307 <sumankumarcse2026@gmail.com>
2025-01-31 22:33:07 +0530

    Add new

---
+++ _09_LINKED_LIST/D02_tut2.java
@@ -0,0 +1,39 @@
+package _09_LINKED_LIST;
+
+import java.util.*;
+
+public class D02_tut2 {
+    public static class Node{
+        int data;   
+        Node next;
+        Node(int data){
+            this.data=data;
+        }
+    }
+
+    public static class LinkedList{
+        Node head = null;
+        Node tail = null;
+    void insertAtEnd(int val){
+        Node temp = new Node(val);
+        if(head == null){           // for empty list
+            head = temp;
+            tail = temp;
+        }
+        else{                       // for available list
+            tail.next = temp;       // present tail ko temp se connect karega
+            tail = temp;            // tail new value ko bana dega
+        }
+    }
+    void Display(){
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data+" ");
+            temp = temp.next;
+        }
+    }
+}
+    public static void main(String[] args) {
+        
+    }
+}
