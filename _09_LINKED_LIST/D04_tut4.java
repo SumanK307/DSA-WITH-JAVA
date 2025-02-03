fcb2ed4cfcbdb501e294f94fd65e2a0ca70f086f
SumanK307 <sumankumarcse2026@gmail.com>
2025-02-03 22:23:28 +0530

    Add new

---
+++ _09_LINKED_LIST/D04_tut4.java
@@ -0,0 +1,16 @@
+package _09_LINKED_LIST;
+
+
+public class D04_tut4 {
+    public static void main(String[] args) {
+        
+        System.out.println("LL tail value is:"+li.tail.data);
+        System.out.println("LL head value is:"+li.head.data);
+        // System.out.println("at index value is:"+li.getIndVal(3));
+        System.out.println("LL Size is:"+ li.size);
+        li.Display();
+        System.out.println("LL Size is:"+ li.size);
+        System.out.println("LL tail value is:"+li.tail.data);
+        System.out.println("LL head value is:"+li.head.data);
+    }    
+}
