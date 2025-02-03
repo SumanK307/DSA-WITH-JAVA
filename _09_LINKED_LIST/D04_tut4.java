4fa24fc84c138d88127feec2b8cf011a716a1d11
SumanK307 <sumankumarcse2026@gmail.com>
2025-02-03 22:24:09 +0530

    Add new

--- _09_LINKED_LIST/D04_tut4.java
+++ _09_LINKED_LIST/D04_tut4.java
@@ -3,7 +3,14 @@ package _09_LINKED_LIST;
 
 public class D04_tut4 {
     public static void main(String[] args) {
-        
+        LinkedList li = new LinkedList();
+        li.insertAtEnd(4);
+        li.insertAtEnd(5);
+        li.insertAtEnd(8);
+        li.insertAtEnd(6);
+        li.insertAtEnd(1);
+        li.insertAtInd(9, 3);
+        li.Display();
         System.out.println("LL tail value is:"+li.tail.data);
         System.out.println("LL head value is:"+li.head.data);
         // System.out.println("at index value is:"+li.getIndVal(3));
