25a6a4d3cf7f6fcfbc189cdfc962854e712c58df
SumanK307 <sumankumarcse2026@gmail.com>
2025-02-11 22:13:14 +0530

    Add new

--- _09_LINKED_LIST/D11_tut11.java
+++ _09_LINKED_LIST/D11_tut11.java
@@ -56,6 +56,15 @@ public class D11_tut11 {
         }
 
     public static void main(String[] args) {
-        
+        Node a = new Node(3);
+        Node b = new Node(5);
+        Node c = new Node(6);
+        Node d = new Node(8);
+        Node e = new Node(16);         
+        a.next = b;                
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        e.next = c;
     }
 }
