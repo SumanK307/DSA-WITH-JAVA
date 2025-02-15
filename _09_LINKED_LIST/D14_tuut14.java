b40b81bb1f550a9e353d90d7ca73d00eccda0e30
SumanK307 <sumankumarcse2026@gmail.com>
2025-02-15 23:47:32 +0530

    Add new

---
+++ _09_LINKED_LIST/D14_tuut14.java
@@ -0,0 +1,41 @@
+package _09_LINKED_LIST;
+
+public class D14_tuut14 {
+    public static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static class LinkedList {
+        Node head = null;
+        Node tail = null;
+        int size = 0;
+
+        void insertAtEnd(int val) {
+            Node temp = new Node(val);
+            if (head == null) {
+                head = temp;
+                tail = temp;
+            } else {
+                tail.next = temp;
+                tail = temp;
+            }
+            size++;
+        }
+
+        void Display(Node head) {
+            Node temp = head;
+            while (temp != null) {
+                System.out.print(temp.data + " ");
+                temp = temp.next;
+            }
+            System.out.println();
+        }
+    public static void main(String[] args) {
+        
+    }
+}
