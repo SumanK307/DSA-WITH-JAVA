3d8bc03db5932b449070c70867cc24eb542d932e
SumanK307 <sumankumarcse2026@gmail.com>
2025-02-18 23:49:47 +0530

    Add new

---
+++ _09_LINKED_LIST/D16_tut16.java
@@ -0,0 +1,15 @@
+package _09_LINKED_LIST;
+
+public class D16_tut16 {
+    public static class Node{
+        int data;
+        Node prev;
+        Node next;
+        Node(int data){
+            this.data = data;
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}
