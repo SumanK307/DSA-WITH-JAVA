c7da160b04a4bfec5a5d7af0c63f66bb1e48baa5
SumanK307 <sumankumarcse2026@gmail.com>
2025-02-18 23:50:16 +0530

    Add new

--- _09_LINKED_LIST/D16_tut16.java
+++ _09_LINKED_LIST/D16_tut16.java
@@ -9,6 +9,19 @@ public class D16_tut16 {
             this.data = data;
         }
     }
+    public static class DLinkedList{
+        Node head = null;
+        Node tail = null;
+        Node prev = null;
+        int size = 0;
+        void display(){
+            Node temp = head;
+            while(temp != null){
+                System.out.print(temp.data+" ");
+                 temp = temp.next;
+            }
+            System.out.println();
+        }
     public static void main(String[] args) {
         
     }
