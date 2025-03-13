1e9160474baeafd88b524d9f49b736ad06bf50e6
SumanK307 <sumankumarcse2026@gmail.com>
2025-03-13 23:45:28 +0530

    Add new

--- _05_ARRAY/D06_Six.java
+++ _05_ARRAY/D06_Six.java
@@ -3,31 +3,29 @@ package _05_ARRAY;
 import java.util.Scanner;
 
 public class D06_Six {
-    static int SqureRoot(int n){
-        int s=0;
-        int e=n;
+    static int SqureRoot(int n) {
+        int s = 0;
+        int e = n;
         int mid;
-        int ans=-1;
-        while(s <= e){
-            mid = s + (e-s)/2;
-            if(n*n == n){
+        int ans = -1;
+        while (s <= e) {
+            mid = s + (e - s) / 2;
+            if (n * n == n) {
                 return mid;
-            }
-            else if(n*n < n){
+            } else if (n * n < n) {
                 ans = mid;
-                s = mid+1;
-            }
-            else{
-                e = mid-1;
+                s = mid + 1;
+            } else {
+                e = mid - 1;
             }
         }
-    return ans;
-        }
+        return ans;
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the value of n:");  
+        System.out.print("Enter the value of n:");
         int n = sc.nextInt();
-        System.out.println("Squre root of "+n+" is :"+SqureRoot(n));
+        System.out.println("Squre root of " + n + " is :" + SqureRoot(n));
     }
 }
