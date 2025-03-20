a14479cd280c97a790453d12def10c9810ed4feb
SumanK307 <sumankumarcse2026@gmail.com>
2025-03-20 23:37:57 +0530

    Add new

--- _05_ARRAY/D14_tut.java
+++ _05_ARRAY/D14_tut.java
@@ -17,7 +17,7 @@ public class D14_tut {
         arr[i][j] = arr[i][j]-arr[j][i];
     }
     static void reverseArray(int[] arr){ 
-        int i=0,j=arr.length-1;
+        int i=0, j=arr.length-1;
         while(i<j){
             arr[i]=arr[i]+arr[j];
             arr[j]=arr[i]-arr[j];
