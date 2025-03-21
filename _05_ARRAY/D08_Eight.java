322ce56d8ebb46b29d20cf80723977342ab4afaf
SumanK307 <sumankumarcse2026@gmail.com>
2025-03-21 22:10:59 +0530

    Add new

--- _05_ARRAY/D08_Eight.java
+++ _05_ARRAY/D08_Eight.java
@@ -16,10 +16,10 @@ public class D08_Eight {
         int[] ans = new int[n];  
         int j = 0;
         for (int i = n-k; i<n ; i++) {
-            ans[j++]=arr[i];
+            ans[j++] = arr[i];
         }
         for (int i = 0; i < n-k; i++) {
-            ans[j++]=arr[i];            
+            ans[j++] = arr[i];            
         }
         return ans;
     }
