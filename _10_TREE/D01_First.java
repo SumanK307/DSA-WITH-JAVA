5ccbf827473fb375b13f79c2611ec4b5147a69ae
SumanK307 <sumankumarcse2026@gmail.com>
2025-03-31 13:18:38 +0530

    Add new

--- _10_TREE/D01_First.java
+++ _10_TREE/D01_First.java
@@ -3,6 +3,20 @@ package _10_TREE;
 import java.util.*;
 
 public class D01_First {
+        static class Node{
+            int data;
+            Node left;
+            Node right;
+            Node(int data){
+                this.data = data; 
+                this.left = null;
+                this.right = null;
+            }
+        }
+        static class BinaryTree{
+            static int idx = -1;
+
+        }
     public static void main(String[] args) {
 
     }
