de93002be923502373e49c6e85da0694c71cc18d
SumanK307 <sumankumarcse2026@gmail.com>
2025-03-31 13:21:36 +0530

    Add new

--- _10_TREE/D01_First.java
+++ _10_TREE/D01_First.java
@@ -28,6 +28,9 @@ public class D01_First {
             }
         }
     public static void main(String[] args) {
-
+        int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+        System.out.println(root.data);
     }
 }
