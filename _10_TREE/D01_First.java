58c7e855e4989c9712e12a7dc25df9f42754a69f
SumanK307 <sumankumarcse2026@gmail.com>
2025-04-02 15:07:42 +0530

    Add new

--- _10_TREE/D01_First.java
+++ _10_TREE/D01_First.java
@@ -72,6 +72,17 @@ public class D01_First {
                         q.add(null);
                     }
                 }
+                else{
+                    System.out.print(currNode.data+" ");
+                    if(currNode.left != null){
+                        q.add(currNode.left);
+                    }
+                    if(currNode.right != null){
+                        q.add(currNode.right);
+                    }
+                }                
+            }
+        }
     }
     public static void main(String[] args) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
