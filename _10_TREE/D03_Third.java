1ef7c412e81cda057045551dbab1b1ba928dff1c
SumanK307 <sumankumarcse2026@gmail.com>
2025-04-06 15:37:14 +0530

    Add new

--- _10_TREE/D03_Third.java
+++ _10_TREE/D03_Third.java
@@ -20,6 +20,19 @@ public class D03_Third {
         }
     }
 
+    // is this subtree
+    public static boolean isSubtree(Node root,Node subRoot){
+        if(root == null)
+            return false;   
+ 
+        if(root.data == subRoot.data){
+            if(isIdentical(root,subRoot))
+                return true;
+        }
+        boolean leftAns = isSubtree(root.left, subRoot);
+        boolean rightAns = isSubtree(root.right, subRoot);
+        return leftAns || rightAns;
+    }
     
     public static void main(String[] args) {
         
