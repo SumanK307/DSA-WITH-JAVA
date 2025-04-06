aaf5f17fe7dc456a5abc7b952e991c83233453bc
SumanK307 <sumankumarcse2026@gmail.com>
2025-04-06 15:36:33 +0530

    Add new

---
+++ _10_TREE/D03_Third.java
@@ -0,0 +1,27 @@
+package _10_TREE;
+
+public class D03_Third {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    static class Info{
+        int dia;
+        int ht;
+        public Info(int dia,int ht){
+            this.dia = dia;
+            this.ht = ht;
+        }
+    }
+
+    
+    public static void main(String[] args) {
+        
+    }
+}
