20d9367b559fdd93184638a5eba76229bf0f3f26
SumanK307 <sumankumarcse2026@gmail.com>
2025-04-16 23:39:34 +0530

    Add new

--- _10_TREE/D04_Four.java
+++ _10_TREE/D04_Four.java
@@ -12,5 +12,8 @@ public class D04_Four {
     public static void main(String[] args) {
         int values[] = {5,1,3,4,2,7};
         Node root = null;
+        for (int i = 0; i < values.length; i++) {
+            root = insert(root,values[i]);
+        }
     }
 }
