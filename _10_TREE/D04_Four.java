ef15f39b64532658c776fccf48339aafb7f95478
SumanK307 <sumankumarcse2026@gmail.com>
2025-04-16 23:38:30 +0530

    Add new

--- _10_TREE/D04_Four.java
+++ _10_TREE/D04_Four.java
@@ -1,6 +1,14 @@
 package _10_TREE;
 
 public class D04_Four {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data = data;
+        }
+    }
     public static void main(String[] args) {
         
     }
