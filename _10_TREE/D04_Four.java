81690f68124405907a0f3e5244abc5b1833e1a7e
SumanK307 <sumankumarcse2026@gmail.com>
2025-04-17 23:50:39 +0530

    Add new

--- _10_TREE/D04_Four.java
+++ _10_TREE/D04_Four.java
@@ -1,19 +1,21 @@
 package _10_TREE;
 
 public class D04_Four {
-    static class Node{
+    static class Node {
         int data;
         Node left;
         Node right;
-        Node(int data){
+
+        Node(int data) {
             this.data = data;
         }
     }
+
     public static void main(String[] args) {
-        int values[] = {5,1,3,4,2,7};
+        int values[] = { 5, 1, 3, 4, 2, 7 };
         Node root = null;
         for (int i = 0; i < values.length; i++) {
-            root = insert(root,values[i]);
+            root = insert(root, values[i]);
         }
     }
 }
