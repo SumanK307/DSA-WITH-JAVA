54fea01ada9a637d4c88b35baf4dcda89852e0c4
SumanK307 <sumankumarcse2026@gmail.com>
2025-04-17 23:49:36 +0530

    Add new

--- _10_TREE/D01_First.java
+++ _10_TREE/D01_First.java
@@ -3,59 +3,61 @@ package _10_TREE;
 import java.util.*;
 
 public class D01_First {
-        static class Node{
-            int data;
-            Node left;
-            Node right;
-            Node(int data){
-                this.data = data; 
-                this.left = null;
-                this.right = null;
-            }
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
         }
-        static class BinaryTree{
-            static int idx = -1;
+    }
 
-            public static Node buildTree(int nodes[]){
-                idx++;
-                if(nodes[idx] == -1){
-                    return null;
-                }
-                Node newNode = new Node(nodes[idx]);
-                newNode.left = buildTree(nodes); 
-                newNode.right = buildTree(nodes); 
-                return newNode;
+    static class BinaryTree {
+        static int idx = -1;
+
+        public static Node buildTree(int nodes[]) {
+            idx++;
+            if (nodes[idx] == -1) {
+                return null;
             }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+            return newNode;
+        }
 
-            public static void preorder(Node root){
-            if(root == null){
+        public static void preorder(Node root) {
+            if (root == null) {
                 return;
             }
-            System.out.print(root.data+" ");
+            System.out.print(root.data + " ");
             preorder(root.left);
             preorder(root.right);
         }
 
-        public static void inorder(Node root){
-            if(root == null){
+        public static void inorder(Node root) {
+            if (root == null) {
                 return;
             }
             inorder(root.left);
-            System.out.print(root.data+" ");
+            System.out.print(root.data + " ");
             inorder(root.right);
         }
 
-        public static void postorder(Node root){
-            if(root == null){
+        public static void postorder(Node root) {
+            if (root == null) {
                 return;
             }
             postorder(root.left);
             postorder(root.right);
-            System.out.print(root.data+" ");
+            System.out.print(root.data + " ");
         }
 
-        public static void levelOrder(Node root){
-            if(root == null){
+        public static void levelOrder(Node root) {
+            if (root == null) {
                 return;
             }
             Queue<Node> q = new LinkedList<>();
@@ -63,29 +65,28 @@ public class D01_First {
             q.add(null);
             while (!q.isEmpty()) {
                 Node currNode = q.remove();
-                if(currNode == null){
+                if (currNode == null) {
                     System.out.println();
-                    if(q.isEmpty()){
+                    if (q.isEmpty()) {
                         break;
-                    }
-                    else{
+                    } else {
                         q.add(null);
                     }
-                }
-                else{
-                    System.out.print(currNode.data+" ");
-                    if(currNode.left != null){
+                } else {
+                    System.out.print(currNode.data + " ");
+                    if (currNode.left != null) {
                         q.add(currNode.left);
                     }
-                    if(currNode.right != null){
+                    if (currNode.right != null) {
                         q.add(currNode.right);
                     }
-                }                
+                }
             }
         }
     }
+
     public static void main(String[] args) {
-        int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
         System.out.println(root.data);
