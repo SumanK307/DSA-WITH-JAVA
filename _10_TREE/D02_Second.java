2ef71efac7b829fdd25dee8a494d1b06e5f47322
SumanK307 <sumankumarcse2026@gmail.com>
2025-04-17 23:50:05 +0530

    Add new

--- _10_TREE/D02_Second.java
+++ _10_TREE/D02_Second.java
@@ -1,39 +1,41 @@
 package _10_TREE;
 
-public class D02_Second{
-    static class Node{
+public class D02_Second {
+    static class Node {
         int data;
         Node left;
         Node right;
-        Node(int data){
+
+        Node(int data) {
             this.data = data;
             this.left = null;
             this.right = null;
         }
     }
-     // Height of tree
-     public static int height(Node root){
+
+    // Height of tree
+    public static int height(Node root) {
         if (root == null) {
             return 0;
         }
         int lh = height(root.left);
         int rh = height(root.right);
-        return Math.max(lh,rh) + 1;
+        return Math.max(lh, rh) + 1;
     }
 
-    //Count of node in tree
-    public static int count(Node root){
-        if(root == null){
+    // Count of node in tree
+    public static int count(Node root) {
+        if (root == null) {
             return 0;
         }
         int leftCnt = count(root.left);
         int rightCnt = count(root.right);
-        return leftCnt+rightCnt+1;
+        return leftCnt + rightCnt + 1;
     }
 
-    //sum of node data 
-    public static int sum(Node root){
-        if(root == null){
+    // sum of node data
+    public static int sum(Node root) {
+        if (root == null) {
             return 0;
         }
         int leftSum = sum(root.left);
@@ -41,17 +43,18 @@ public class D02_Second{
         return leftSum + rightSum + root.data;
     }
 
-    public static int diameter(Node root){
-        if(root == null){
+    public static int diameter(Node root) {
+        if (root == null) {
             return 0;
         }
         int leftDia = diameter(root.left);
         int leftHt = height(root.left);
         int rightDia = diameter(root.right);
         int rightHt = height(root.right);
-        int selfDia = leftHt+rightHt+1;
-        return Math.max(selfDia,Math.max(leftDia,rightDia));
+        int selfDia = leftHt + rightHt + 1;
+        return Math.max(selfDia, Math.max(leftDia, rightDia));
     }
+
     public static void main(String[] args) {
         Node root = new Node(1);
         root.left = new Node(2);
