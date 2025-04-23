342aa18053a4f8a40fcbbeee3d2f0c77e6e5e355
SumanK307 <sumankumarcse2026@gmail.com>
2025-04-23 23:41:15 +0530

    Add new

--- _10_TREE/D05_Five.java
+++ _10_TREE/D05_Five.java
@@ -1,6 +1,15 @@
 package _10_TREE;
 
 public class D05_Five {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
     public static void main(String[] args) {
         
     }
