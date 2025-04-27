708576ca5acca2527a338671321c440288a25c97
SumanK307 <sumankumarcse2026@gmail.com>
2025-04-27 13:29:21 +0530

    Add new

--- _10_TREE/D06_Six.java
+++ _10_TREE/D06_Six.java
@@ -1,6 +1,6 @@
 package _10_TREE;
 
-public class D06_Six{
+public class D06_Six {
     static class Node {
         int data;
         Node left;
@@ -10,7 +10,8 @@ public class D06_Six{
             this.data = data;
         }
     }
-    public static void main(String[] args){
-        int arr[] = {3,5,6,8,10,11,12};
+
+    public static void main(String[] args) {
+        int arr[] = { 3, 5, 6, 8, 10, 11, 12 };
     }
 }
