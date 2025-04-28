004a728f5fd4e4a4ac5ac6cbb1bdeb04be639359
SumanK307 <sumankumarcse2026@gmail.com>
2025-04-28 14:41:37 +0530

    Add new

--- _10_TREE/D06_Six.java
+++ _10_TREE/D06_Six.java
@@ -14,6 +14,10 @@ public class D06_Six {
         if (root == null) {
             return;
         }
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
 
     public static void main(String[] args) {
         int arr[] = { 3, 5, 6, 8, 10, 11, 12 };
