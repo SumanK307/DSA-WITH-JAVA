0bbe2da1e747e371a9bc579a5f2537ff9589ca0d
SumanK307 <sumankumarcse2026@gmail.com>
2025-04-28 14:42:02 +0530

    Add new

--- _10_TREE/D06_Six.java
+++ _10_TREE/D06_Six.java
@@ -18,6 +18,15 @@ public class D06_Six {
         preorder(root.left);
         preorder(root.right);
     }
+    public static Node createBST(int arr[],int st,int end){
+        if(st>end)
+            return null;
+        int mid = (st+end)/2;
+        Node root = new Node(arr[mid]);
+        root.left = createBST(arr, st, mid-1);
+        root.right = createBST(arr, mid+1,end);
+        return root;
+    }
 
     public static void main(String[] args) {
         int arr[] = { 3, 5, 6, 8, 10, 11, 12 };
