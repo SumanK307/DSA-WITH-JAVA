05878708dccde142176a77b8fe10e96c0fa26900
SumanK307 <sumankumarcse2026@gmail.com>
2025-04-29 14:02:33 +0530

    Add new

--- _10_TREE/D07_Seven.java
+++ _10_TREE/D07_Seven.java
@@ -2,6 +2,17 @@ package _10_TREE;
 import java.util.ArrayList;
 
 public class D07_Seven {
+
+        static class Node {
+            int data;
+            Node left;
+            Node right;
+            Node(int data) {
+                this.data = data;
+                this.left = null;
+                this.right = null;
+            }
+        }
     public static void main(String[] args) {
         
     }
