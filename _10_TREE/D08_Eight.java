c769e59d6bf9166947b9a59355ad833c89792462
SumanK307 <sumankumarcse2026@gmail.com>
2025-05-03 18:40:48 +0530

    Add new

--- _10_TREE/D08_Eight.java
+++ _10_TREE/D08_Eight.java
@@ -1,6 +1,13 @@
 package _10_TREE;
 
 public class D08_Eight {
+
+    Node(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
     public static void main(String[] args) {
         
     }
