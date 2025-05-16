b85c2d0bd0195dde785b779e896791d08306b735
SumanK307 <sumankumarcse2026@gmail.com>
2025-05-16 23:31:27 +0530

    Add new

--- _12_GRAPH/D02_Second.java
+++ _12_GRAPH/D02_Second.java
@@ -3,7 +3,7 @@ package _12_GRAPH;
 public class D02_Second {
     public class Student{
         Student{
-            
+            int x;
         }
     }
     public static void main(String[] args) {
