82f84c096f0cab2a4d21fabac06d2cbef389884f
SumanK307 <sumankumarcse2026@gmail.com>
2025-05-16 23:31:03 +0530

    Add new

--- _12_GRAPH/D02_Second.java
+++ _12_GRAPH/D02_Second.java
@@ -2,7 +2,9 @@ package _12_GRAPH;
 
 public class D02_Second {
     public class Student{
-        
+        Student{
+            
+        }
     }
     public static void main(String[] args) {
         
