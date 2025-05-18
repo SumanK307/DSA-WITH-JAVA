50d6739ccb5b04e068e3e9c2bef06260e276f807
SumanK307 <sumankumarcse2026@gmail.com>
2025-05-18 23:33:35 +0530

    Add new

--- _12_GRAPH/D01_First.java
+++ _12_GRAPH/D01_First.java
@@ -4,7 +4,10 @@ public class D01_First {
     public static class Node {
         int data;
         Node next;
-        
+        Node(int data){
+            this.data = data;
+        }
+    }
     public static void main(String[] args) {
         int n;
         n=1;
