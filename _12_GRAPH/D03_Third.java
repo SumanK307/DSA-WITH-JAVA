c137da23c50170112626b531585c295a46ab2cbf
SumanK307 <sumankumarcse2026@gmail.com>
2025-05-20 23:18:08 +0530

    Add new

--- _12_GRAPH/D03_Third.java
+++ _12_GRAPH/D03_Third.java
@@ -1,6 +1,11 @@
 package _12_GRAPH;
 
 public class D03_Third {
+    
+    Node(int data){
+            this.data = data;
+        }
+    }
     public static void main(String[] args) {
         int n;
     }
