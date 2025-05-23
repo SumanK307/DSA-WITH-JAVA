4781d9baece1a2953c589cadcb35d6ac9153919c
SumanK307 <sumankumarcse2026@gmail.com>
2025-05-23 10:22:12 +0530

    Add new

--- _12_GRAPH/D04_Fourth.java
+++ _12_GRAPH/D04_Fourth.java
@@ -4,7 +4,10 @@ public class D04_Fourth {
     public static class Node {
         int data;
         Node next;
-
+        Node(int data){
+            this.data = data;
+        }
+    }
     public static void main(String[] args) {
         
     }
